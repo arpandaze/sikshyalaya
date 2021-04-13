1b4dccff100828a6ebb8aa033099378bf9320a92
Daze <dazehere@yandex.com>
2021-04-13 17:57:01 +0545

    Models and CRUD updated

--- backend/app/app/crud/crud_user.py
+++ backend/app/app/crud/crud_user.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 
 from app.core.security import get_password_hash, verify_password
 from app.crud.base import CRUDBase
+from app.crud.crud_course import crud_course
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
 
@@ -13,12 +14,19 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         return db.query(User).filter(User.email == email).first()
 
     def create(self, db: Session, *, obj_in: UserCreate) -> User:
+        courses = list(map(lambda id: crud_course.get(db=db, id=id), obj_in.course))
         db_obj = User(
-            email=obj_in.email,
-            hashed_password=get_password_hash(obj_in.password),
-            full_name=obj_in.full_name,
-            is_superuser=obj_in.is_superuser,
-            auth_provider=obj_in.auth_provider
+            email=obj_in.email,  # noqa
+            hashed_password=get_password_hash(obj_in.password),  # noqa
+            full_name=obj_in.full_name,  # noqa
+            is_superuser=obj_in.is_superuser,  # noqa
+            auth_provider=obj_in.auth_provider,  # noqa
+            dob=obj_in.dob,  # noqa
+            enrolled_course=courses,  # noqa
+            contact_number=obj_in.contact_number,  # noqa
+            address=obj_in.address,  # noqa
+            is_teacher=obj_in.is_teacher,  # noqa
+            sem=obj_in.sem  # noqa
         )
         db.add(db_obj)
         db.commit()
@@ -26,7 +34,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         return db_obj
 
     def update(
-        self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
+            self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
     ) -> User:
         if isinstance(obj_in, dict):
             update_data = obj_in
--- backend/app/app/models/school.py
+++ backend/app/app/models/school.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
 
-
 if TYPE_CHECKING:
     from .item import Item  # noqa: F401
 
@@ -15,4 +14,4 @@ class School(Base):
     name = Column(String(length=128), nullable=False)
     address = Column(String(length=64))
     department = relationship("Department", back_populates="school")
-    __tablename__ = "school"
+    __tablename__ = "school" # noqa
