382f3a36482b99c32eb9e72d165ea9fff83ea3bf
Rushab11 <humagainrushab@gmail.com>
2021-04-14 14:21:57 +0545

    Sidebar test

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,18 +1,85 @@
 import React, { useState } from "react";
 import Button from "../components/Button";
 import theme from "../utils/colors";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiFillDashboard } from "react-icons/ai";
+import { FaArrowLeft } from "react-icons/fa";
 import Grid from "@material-ui/core/Grid";
-import Image from "../components/Image";
+import Box from "@material-ui/core/Box";
 
 const styleSheet = {
-  sideBar: {
-    height: "100vh",
-    backgroundColor: theme.white,
-  },
+	sidebarClose: {
+		width: "60px",
+		height: "100vh",
+		backgroundColor: theme.white,
+		display: "flex",
+		backgroundColor: "transparent",
+		transition: "500ms",
+		boxShadow: "2px 2px 13px -3px rgba(0,0,0,0.32)",
+	},
+	sidebarOpen: {
+		width: "150px",
+		height: "100vh",
+		backgroundColor: theme.white,
+		display: "flex",
+		backgroundColor: "transparent",
+		justifyContent: "right",
+		boxShadow: "2px 2px 13px -3px rgba(0,0,0,0.32)",
+		transition: "500ms",
+	},
+	iconStyle: {},
 };
 
+const sidebarItems = [
+	{
+		title: "Dashboard",
+		route: "/dashboard",
+		icon: <AiFillDashboard size={25} />,
+	},
+	{
+		title: "Dashboard",
+		route: "/dashboard",
+		icon: <AiFillDashboard size={25} />,
+	},
+	{
+		title: "Dashboard",
+		route: "/dashboard",
+		icon: <AiFillDashboard size={25} />,
+	},
+];
+
 const SideBar = () => {
-  return <div style={styleSheet.sideBar}></div>;
+	const [sidebarOpen, setSidebarOpen] = useState(false);
+	const sidebarHandler = () => {
+		!sidebarOpen ? setSidebarOpen(true) : setSidebarOpen(false);
+	};
+	return (
+		<Grid
+			container
+			direction="column"
+			justify="center"
+			alignItems="flex-start"
+			style={styleSheet.sidebarClose}
+		>
+			<Box>
+				<GiHamburgerMenu size="25" onClick={sidebarHandler} />
+				{console.log(sidebarOpen)}
+			</Box>
+		</Grid>
+
+		// {/* <List display="flex-start">
+		//       <ListItem button onClick={sidebarHandler} style={sidebarOpen?styleSheet.sidebarOpen:styleSheet.sidebarClose}>
+		//             <ListItemIcon disableRipple={true}>
+		//                   {sidebarOpen?<FaArrowLeft size="25" style={styleSheet.hamburgerIcon}/>:<GiHamburgerMenu size="25"/>}
+		//             </ListItemIcon>
+		//       </ListItem>
+		// </List> */}
+	);
 };
 
 export default SideBar;
