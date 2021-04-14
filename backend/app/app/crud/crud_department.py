6493dd9e685448be4fe5f2397aea8263e5b26381
Daze <dazehere@yandex.com>
2021-04-14 12:58:50 +0545

    CRUD for models defined

--- backend/app/app/crud/crud_department.py
+++ backend/app/app/crud/crud_department.py
@@ -1,17 +1,14 @@
-from typing import List
-
-from fastapi.encoders import jsonable_encoder
-from sqlalchemy.orm import Session
-
 from app.crud.base import CRUDBase
 from app.models.department import Department
 from app.schemas.department import (
     DepartmentCreate,
     DepartmentUpdate,
-    DepartmentDelete,
     DepartmentRetrieve,
 )
 
 
-class CRUDSchool(CRUDBase[DepartmentRetrieve, DepartmentCreate, DepartmentUpdate]):
+class CRUDDepartment(CRUDBase[DepartmentRetrieve, DepartmentCreate, DepartmentUpdate]):
     pass
+
+
+crud_department = CRUDDepartment(Department)
