0b597fab027b14988e0f56b15235f260b5c6c0d7
Daze <dazehere@yandex.com>
2021-04-14 13:07:27 +0545

    Endpoint files created

---
+++ backend/app/app/api/api_v1/endpoints/personal_note.py
@@ -0,0 +1,18 @@
+from typing import Any, List
+
+from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi.encoders import jsonable_encoder
+from pydantic.networks import EmailStr
+from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.api import deps
+from app.core.config import settings
+from app.utils import send_new_account_email
+
+
+from app.crud.crud_school import crud_school
+
+router = APIRouter()
+
+
