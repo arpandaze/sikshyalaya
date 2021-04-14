47e675cfbde45e1b354014ca5e2cc4ccd836c66c
Daze <dazehere@yandex.com>
2021-04-14 14:14:33 +0545

    Minor update

--- backend/app/app/api/api_v1/endpoints/class_session.py
+++ backend/app/app/api/api_v1/endpoints/class_session.py
@@ -1,15 +1,24 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
+from app import models
 from app.api import deps
 from app.crud.crud_school import crud_school
-from typing import List
+from app.schemas import ClassSession
 
 router = APIRouter()
 
 
-@router.get("/class_session", response_model=List[int])
-def get_class_session(db: Session = Depends(deps.get_db)):
+@router.get(
+    "/class_session",
+    response_model=List[ClassSession],
+)
+def get_class_session(
+    db: Session = Depends(deps.get_db),
+    current_user: models.User = Depends(deps.get_current_active_user),
+):
     school = crud_school.get(db=db, id=2)
     return school
 
