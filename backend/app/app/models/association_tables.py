3aec014e2133750b0490183998a426451802e7cb
Daze <dazehere@yandex.com>
2021-04-14 12:57:14 +0545

    Association table for user and class session created

--- backend/app/app/models/association_tables.py
+++ backend/app/app/models/association_tables.py
@@ -7,3 +7,9 @@ user_course_association_table = Table(
     Column("course_id", Integer, ForeignKey("course.id")),
     Column("user_id", Integer, ForeignKey("user.id"))
 )
+
+user_class_session_association_table = Table(
+    "association", Base.metadata,
+    Column("class_session_id", Integer, ForeignKey("class_session.id")),
+    Column("user_id", Integer, ForeignKey("user.id"))
+)
