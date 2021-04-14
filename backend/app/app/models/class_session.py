ab22c3922780456db64747aefd582201409b9e98
Daze <dazehere@yandex.com>
2021-04-14 12:58:28 +0545

    Instructor can be multiple

--- backend/app/app/models/class_session.py
+++ backend/app/app/models/class_session.py
@@ -4,6 +4,7 @@ from sqlalchemy import Boolean, Column, Integer, String, Enum, DateTime, Foreign
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
+from association_tables import user_class_session_association_table
 
 import enum
 
@@ -17,8 +18,9 @@ if TYPE_CHECKING:
 class ClassSession(Base):
     id = Column(Integer, primary_key=True)
     datetime = Column(DateTime)
-    instructor: Column()
+    is_active = Column(Boolean)
+    instructors = relationship("User", secondary=user_class_session_association_table, backref="class_session")
     course_id = Column(Integer, ForeignKey("course.id"))
     course = relationship("Course", backref="session")
     description = Column(String(length=128))
-    __tablename__ = "classsession"
+    __tablename__ = "class_session"
