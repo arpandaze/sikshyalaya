73206718c6658a3c670ad67ea877a03dbeb75cca
Daze <dazehere@yandex.com>
2021-04-17 11:30:18 +0545

    Population script added

---
+++ backend/app/app/tests/populate.py
@@ -0,0 +1,10 @@
+from app.crud import (
+    crud_personal_note,
+    crud_user,
+    crud_course,
+    crud_course,
+    crud_class_session,
+    crud_department,
+    crud_teacher_note,
+    crud_group,
+)
