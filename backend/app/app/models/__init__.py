a359835a7f5ba35e9cc4288864edf7d88874f466
Daze <dazehere@yandex.com>
2021-04-17 11:26:32 +0545

    Item model, crud and endpoint deleted

--- backend/app/app/models/__init__.py
+++ backend/app/app/models/__init__.py
@@ -1,7 +1,6 @@
 from .class_session import ClassSession  # noqa: F401
 from .course import Course  # noqa: F401
 from .department import Department  # noqa: F401
-from .item import Item  # noqa: F401
 from .personal_note import PersonalNote  # noqa: F401
 from .program import Program  # noqa: F401
 from .school import School  # noqa: F401
