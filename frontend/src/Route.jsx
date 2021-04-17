f403d615a7821f4be350b29b4161a9b57205d05d
Rushab11 <humagainrushab@gmail.com>
2021-04-17 14:47:15 +0545

    Fixed minor routing problem

--- frontend/src/Route.jsx
+++ frontend/src/Route.jsx
@@ -4,17 +4,19 @@ import Login from "./views/Login/Login";
 import Dashboard from "./views/Dashboard/Dashboard";
 import Notes from "./views/Notes/Notes";
 import Quiz from "./views/Quiz/Quiz";
+import Profile from "./views/Profile/Profile";
 
 const Routes = () => {
-  return (
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/quiz" component={Quiz} />
-      <Route path="/notes" component={Notes} />
-      <Redirect from="/" to="dashboard" />
-    </Switch>
-  );
+	return (
+		<Switch>
+			<Route path="/login" component={Login} />
+			<Route path="/dashboard" component={Dashboard} />
+			<Route path="/quiz" component={Quiz} />
+			<Route path="/note" component={Notes} />
+			<Route path="/profile" component={Profile} />
+			<Redirect exact from="/" to="dashboard" />
+		</Switch>
+	);
 };
 
 export default Routes;
