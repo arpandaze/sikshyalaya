e4bf9f67f4ed4f9dda9d4a738e468cecedc247f8
Daze <dazehere@yandex.com>
2021-04-18 08:37:52 +0545

    Department linked to course

--- backend/app/app/models/course.py
+++ backend/app/app/models/course.py
@@ -1,19 +1,12 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import Boolean, Column, Integer, String, Enum, DateTime
-from sqlalchemy.orm import relationship
-
 from app.db.base_class import Base
-
-import enum
-
-from app.core.config import settings
-
-
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Course(Base):
     id = Column(Integer, primary_key=True)
     course_code = Column(String, index=True, unique=True)
     course_name = Column(String(128), nullable=False)
+    department_id = Column(Integer, ForeignKey("department.id"))
+    department = relationship("Department", backref="courses")
     __tablename__ = "course"
