2ce6f7d85635960b7e2b5456abaeaed74c1a0d4e
Daze <dazehere@yandex.com>
2021-04-18 08:36:44 +0545

    Group imported to __init__.py

--- backend/app/app/models/__init__.py
+++ backend/app/app/models/__init__.py
@@ -1,6 +1,7 @@
 from .class_session import ClassSession  # noqa: F401
 from .course import Course  # noqa: F401
 from .department import Department  # noqa: F401
+from .group import Group  # noqa: F401
 from .personal_note import PersonalNote  # noqa: F401
 from .program import Program  # noqa: F401
 from .school import School  # noqa: F401
