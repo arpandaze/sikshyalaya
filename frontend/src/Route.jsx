9fad05b4abda3864bc05ae493b7eb72475384e05
waterlulu12 <aatishshrestha123@gmail.com>
2021-04-18 08:34:26 +0545

    minor change

--- frontend/src/Route.jsx
+++ frontend/src/Route.jsx
@@ -3,24 +3,23 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import Login from "./views/Login/Login";
 import Dashboard from "./views/Dashboard/Dashboard";
 import Notes from "./views/Notes/Notes";
-import Quiz from "./views/Quiz/Quiz";
 import Landing from "./views/Landing/Landing";
 import Quiz from "./views/Quiz/Quiz";
 import Profile from "./views/Profile/Profile";
 
 const Routes = () => {
-	return (
-		<Switch>
-			<Route path="/login" component={Login} />
-			<Route path="/dashboard" component={Dashboard} />
-			<Route path="/quiz" component={Quiz} />
-			<Route path="/note" component={Notes} />
-			<Route path="/landing" component={Landing} />
-			<Route path="/profile" component={Profile} />
+  return (
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/quiz" component={Quiz} />
+      <Route path="/note" component={Notes} />
+      <Route path="/landing" component={Landing} />
+      <Route path="/profile" component={Profile} />
 
-			<Redirect exact from="/" to="landing" />
-		</Switch>
-	);
+      <Redirect exact from="/" to="landing" />
+    </Switch>
+  );
 };
 
 export default Routes;
