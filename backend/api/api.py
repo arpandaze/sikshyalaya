a5b40631dbdfa674a914f228cb21ac6b6911721c
Daze <dazehere@yandex.com>
2021-04-22 17:36:38 +0545

    Endpoint tags changed and user_permission endpoints added

--- backend/api/api.py
+++ backend/api/api.py
@@ -5,22 +5,23 @@ from api.endpoints import (
     teacher_note,
     users,
 )
-from api.endpoints import login, utils, course, school, department, class_session, personal_note
+from api.endpoints import login, utils, course, school, department, class_session, personal_note, user_permission
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-api_router.include_router(school.router, prefix="/school", tags=["schools"])
-api_router.include_router(course.router, prefix="/course", tags=["courses"])
-api_router.include_router(department.router, prefix="/deparment", tags=["departments"])
+api_router.include_router(login.router, tags=["Login"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"])
+api_router.include_router(utils.router, prefix="/utils", tags=["Utils"])
+api_router.include_router(school.router, prefix="/school", tags=["Schools"])
+api_router.include_router(course.router, prefix="/course", tags=["Courses"])
+api_router.include_router(department.router, prefix="/deparment", tags=["Departments"])
 api_router.include_router(
-    class_session.router, prefix="/class_session", tags=["class_sessions"]
+    class_session.router, prefix="/class_session", tags=["Class Sessions"]
 )
 api_router.include_router(
-    personal_note.router, prefix="/personal_note", tags=["personal_notes"]
+    personal_note.router, prefix="/personal_note", tags=["Personal Notes"]
 )
-api_router.include_router(program.router, prefix="/program", tags=["programs"])
+api_router.include_router(program.router, prefix="/program", tags=["Programs"])
+api_router.include_router(user_permission.router, prefix="/user_permission", tags=["User Permission"])
 api_router.include_router(
-    teacher_note.router, prefix="/teacher_note", tags=["teacher_notes"]
+    teacher_note.router, prefix="/teacher_note", tags=["Teacher Notes"]
 )
