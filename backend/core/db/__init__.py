a09292d6732ef9076b2b7867572f4064a1bfbe20
Daze <dazehere@yandex.com>
2021-04-22 19:40:07 +0545

    Redis general client added

--- backend/core/db/__init__.py
+++ backend/core/db/__init__.py
@@ -1,6 +1,7 @@
 from .base import Base
 from .redis_session import redis_blacklist_client
 from .redis_session import redis_cache_client
+from .redis_session import redis_general
 from .redis_session import redis_session_client
 from .redis_session import redis_throttle_client
 from .session import SessionLocal
