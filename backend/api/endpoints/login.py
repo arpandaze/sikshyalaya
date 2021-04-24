c01791bbd74b33e8389ff1e93cd0ecb2104aa6cb
Daze <dazehere@yandex.com>
2021-04-24 15:40:17 +0545

    minor refactor

--- backend/api/endpoints/login.py
+++ backend/api/endpoints/login.py
@@ -1,4 +1,3 @@
-from datetime import timedelta
 from typing import Any
 
 from fastapi import (
@@ -6,53 +5,43 @@ from fastapi import (
     Body,
     Depends,
     HTTPException,
-    Response,
-    Form,
-    status,
     Request,
 )
-from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
 import cruds
 import models
 import schemas
-
-from utils import deps
-from core import security
 from core import throttle
-from core.config import settings
 from core.security import get_password_hash, create_sesssion_token
+from forms.login import LoginForm
+from utils import deps
 from utils.utils import (
     generate_password_reset_token,
     send_reset_password_email,
     verify_password_reset_token,
 )
-from forms.login import LoginForm
-
 
 router = APIRouter()
 
 
 @router.post("/auth/web", response_model=schemas.Token)
 async def login_web_session(
-    db: Session = Depends(deps.get_db), form_data: LoginForm = Depends()
+        db: Session = Depends(deps.get_db), form_data: LoginForm = Depends()
 ) -> Any:
-    """
-    OAuth2 compatible token login, get an access token for future requests
-    """
     if not form_data.username:
         form_data.username = form_data.email
 
     user = cruds.crud_user.authenticate(
         db, email=form_data.username, password=form_data.password
     )
+
     if not user:
         raise HTTPException(status_code=401, detail="Incorrect email or password")
     elif not user.is_active:
         raise HTTPException(status_code=401, detail="Inactive user")
-    session_token = await create_sesssion_token(user)
+    session_token = await create_sesssion_token(user, form_data.remember_me)
     response = JSONResponse({"status": "success"})
     response.set_cookie("session", session_token, httponly=True)
     return response
@@ -60,7 +49,7 @@ async def login_web_session(
 
 @router.get("/auth/web/test")
 async def test_session_token(
-    current_user: models.User = Depends(deps.get_current_user),
+        current_user: models.User = Depends(deps.get_current_user),
 ) -> Any:
     return current_user.email
 
@@ -77,26 +66,24 @@ async def recover_password(email: str, db: Session = Depends(deps.get_db)) -> An
             status_code=404,
             detail="The user with this username does not exist in the system.",
         )
-    password_reset_token = generate_password_reset_token(email=email)
+    password_reset_token = await generate_password_reset_token(uid=user.id)
     send_reset_password_email(
-        email_to=user.email, email=email, token=password_reset_token
+        email_to=user.email, email=user.email, token=password_reset_token
     )
     return {"msg": "Password recovery email sent"}
 
 
 @router.post("auth/reset-password/", response_model=schemas.Msg)
 async def reset_password(
-    token: str = Body(...),
-    new_password: str = Body(...),
-    db: Session = Depends(deps.get_db),
+        token: str = Body(...),
+        new_password: str = Body(...),
+        db: Session = Depends(deps.get_db),
 ) -> Any:
     """
     Reset password
     """
-    email = verify_password_reset_token(token)
-    if not email:
-        raise HTTPException(status_code=400, detail="Invalid token")
-    user = cruds.crud_user.get_by_email(db, email=email)
+    uid = await verify_password_reset_token(token)
+    user = cruds.crud_user.get_by_id(db, id=uid)
     if not user:
         raise HTTPException(
             status_code=404,
@@ -114,9 +101,9 @@ async def reset_password(
 @router.get("/thtest1")
 @throttle.ip_throttle(rate=10, per=60)
 async def throttle_test(
-    request: Request,
-    db: Session = Depends(deps.get_db),
-    current_user: models.User = Depends(deps.get_current_user),
+        request: Request,
+        db: Session = Depends(deps.get_db),
+        current_user: models.User = Depends(deps.get_current_user),
 ):
     return "Throttle test endpoint 1 Hello"
 
@@ -124,8 +111,8 @@ async def throttle_test(
 @router.get("/thtest2")
 @throttle.user_throttle(rate=20, per=60)
 async def throttle_test1(
-    request: Request,
-    db: Session = Depends(deps.get_db),
-    current_user: models.User = Depends(deps.get_current_user),
+        request: Request,
+        db: Session = Depends(deps.get_db),
+        current_user: models.User = Depends(deps.get_current_user),
 ):
     return "Throttle test endpoint 2"
--- backend/utils/deps.py
+++ backend/utils/deps.py
@@ -1,22 +1,16 @@
-from typing import Any, Optional
 from typing import Generator
+from typing import Optional
 
+from fastapi import Cookie
 from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
-from pydantic import ValidationError
 from sqlalchemy.orm import Session
 
 import cruds
 import models
-import schemas
-from core import security
 from core import settings
 from core.db import SessionLocal
-from core.db import redis_blacklist_client
 from core.db import redis_session_client
-from asyncio import run
-from fastapi import Cookie
+
 
 def get_db() -> Generator:
     try:
@@ -27,8 +21,8 @@ def get_db() -> Generator:
 
 
 async def get_current_user(
-    db: Session = Depends(get_db),
-    session: Optional[str] = Cookie(None),
+        db: Session = Depends(get_db),
+        session: Optional[str] = Cookie(None),
 ) -> models.User:
     try:
         user_id = await redis_session_client.client.get(session, encoding="utf-8")
@@ -47,7 +41,7 @@ async def get_current_user(
 
 
 def get_current_active_user(
-    current_user: models.User = Depends(get_current_user),
+        current_user: models.User = Depends(get_current_user),
 ) -> models.User:
     if not cruds.crud_user.is_active(current_user):
         raise HTTPException(status_code=400, detail="Inactive user")
@@ -55,7 +49,7 @@ def get_current_active_user(
 
 
 def get_current_active_teacher(
-    current_user: models.User = Depends(get_current_user),
+        current_user: models.User = Depends(get_current_user),
 ) -> models.User:
     if current_user.user_type == settings.UserType.TEACHER:
         return current_user
@@ -64,7 +58,7 @@ def get_current_active_teacher(
 
 
 def get_current_active_superuser(
-    current_user: models.User = Depends(get_current_user),
+        current_user: models.User = Depends(get_current_user),
 ) -> models.User:
     if not cruds.crud_user.is_superuser(current_user):
         raise HTTPException(
