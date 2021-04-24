e0ca371137f2022e2de08ec298ce59ead3e5d265
Daze <dazehere@yandex.com>
2021-04-24 13:14:35 +0545

    minor changes

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -88,13 +88,7 @@ def send_new_account_email(email_to: str, username: str, password: str) -> None:
 
 
 def generate_password_reset_token(email: str) -> str:
-    delta = timedelta(hours=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS)
-    now = datetime.utcnow()
-    expires = now + delta
-    exp = expires.timestamp()
-    encoded_jwt = jwt.encode(
-        {"exp": exp, "nbf": now, "sub": email}, settings.SECRET_KEY, algorithm="HS256",
-    )
+
     return encoded_jwt
 
 
