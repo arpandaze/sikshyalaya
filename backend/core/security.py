fa35efd322303e626209a74ff7e1fc0ca9952cb5
Daze <dazehere@yandex.com>
2021-04-24 15:39:22 +0545

    create_session_token implemented

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,39 +1,28 @@
 import binascii
 import os
-from datetime import datetime, timedelta
-from typing import Any, Union
 
-from jose import jwt
 from passlib.context import CryptContext
 
 from core.config import settings
 from core.db import redis_session_client
 from models import User
+import json
 
-# from redis import Redis
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-ALGORITHM = "HS256"
 
-
-def create_access_token(
-        subject: Union[str, Any], expires_delta: timedelta = None
-) -> str:
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-    to_encode = {"exp": expire, "sub": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def create_sesssion_token(user: User) -> str:
+async def create_sesssion_token(user: User, remember_me: bool) -> str:
     session_token = binascii.hexlify(os.urandom(20)).decode()
-    await redis_session_client.client.set(session_token, user.id, expire=settings.SESSION_EXPIRE_MINUTES * 60)
+    expire_time = settings.SESSION_EXPIRE_TIME_EXTENDED if remember_me else settings.SESSION_EXPIRE_TIME
+    permissions = list(map(lambda x: x.name, user.permission))
+    data = {
+        session_token: user.id,
+        f"{user.id}_permissions": json.dumps(permissions),
+    }
+    await redis_session_client.client.mset(data)
+    await redis_session_client.client.expire(session_token, expire_time)
+    await redis_session_client.client.expire(f"{user.id}_permissions", expire_time)
     return session_token
 
 
