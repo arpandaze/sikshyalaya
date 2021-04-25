d03106212eb4f0729d732128cfb3d88e9d196187
Daze <dazehere@yandex.com>
2021-04-25 22:05:39 +0545

    Join year field added

--- backend/models/group.py
+++ backend/models/group.py
@@ -1,5 +1,5 @@
 from core.db import Base
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, ForeignKey, SmallInteger
 from sqlalchemy.orm import relationship
 
 
@@ -8,5 +8,6 @@ class Group(Base):
     id = Column(Integer, primary_key=True)
     program_id = Column(Integer, ForeignKey("program.id"))
     program = relationship("Program", backref="groups")
-    sem = Column(Integer)
+    sem = Column(SmallInteger)
+    join_year = Column(SmallInteger)
     __tablename__ = "group"
