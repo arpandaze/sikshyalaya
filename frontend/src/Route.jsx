55cf2d21bd8392ac2df1c0d2efd55efd7361cc92
avgt93 <abhijeetpoudel@gmail.com>
2021-04-25 14:58:08 +0545

    Route added for TeacherDashboard

--- frontend/src/Route.jsx
+++ frontend/src/Route.jsx
@@ -6,19 +6,21 @@ import Notes from "./views/Notes/Notes";
 import Landing from "./views/Landing/Landing";
 import Quiz from "./views/Quiz/Quiz";
 import Profile from "./views/Profile/Profile";
+import TeacherDashboard from "./views/TeacherDashboard/TeacherDashboard";
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/quiz" component={Quiz} />
-      <Route path="/note" component={Notes} />
-      <Route path="/landing" component={Landing} />
-      <Route path="/profile" component={Profile} />
+		<Switch>
+			<Route path="/login" component={Login} />
+			<Route path="/dashboard" component={Dashboard} />
+			<Route path="/quiz" component={Quiz} />
+			<Route path="/note" component={Notes} />
+			<Route path="/landing" component={Landing} />
+			<Route path="/profile" component={Profile} />
+			<Route path="/teacherdashboard" component={TeacherDashboard} />
 
-      <Redirect exact from="/" to="landing" />
-    </Switch>
+			<Redirect exact from="/" to="landing" />
+		</Switch>
   );
 };
 
