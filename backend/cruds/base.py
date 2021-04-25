eac9b296db5b2869493b6098a9873479d9355b82
Daze <dazehere@yandex.com>
2021-04-25 16:30:16 +0545

    Get self added and added req_user param to functions

--- backend/cruds/base.py
+++ backend/cruds/base.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from core.db import Base
-from core.permission import check_permission
+from core.permission.permission import check_permission
 from models import User
 
 ModelType = TypeVar("ModelType", bound=Base)
@@ -26,18 +26,24 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         self.model = model
 
     @check_permission
-    def get(self, db: Session, id: Any) -> Optional[ModelType]:
+    def get(self, db: Session, id: Any, req_user: User) -> Optional[ModelType]:
         return db.query(self.model).filter(self.model.id == id).first()
+
+    def get_self(self, db: Session, req_user: User) -> Optional[ModelType]:
+        return db.query(self.model).filter(self.model.id == req_user.id).first()
 
     @check_permission
     def get_multi(
-        self, db: Session, *, skip: int = 0, limit: int = 100
+            self, db: Session, *, skip: int = 0, limit: int = 100, req_user: User,
     ) -> List[ModelType]:
-        return db.query(self.model).offset(skip).limit(limit).all()
+        if limit == -1:
+            return db.query(self.model).offset(skip).all()
+        else:
+            return db.query(self.model).offset(skip).limit(limit).all()
 
     @check_permission
     def create(
-        self, db: Session, *, obj_in: CreateSchemaType, req_user: User
+            self, db: Session, *, obj_in: CreateSchemaType, req_user: User
     ) -> ModelType:
         obj_in_data = jsonable_encoder(obj_in)
         db_obj = self.model(**obj_in_data)  # type: ignore
@@ -48,11 +54,12 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
 
     @check_permission
     def update(
-        self,
-        db: Session,
-        *,
-        db_obj: ModelType,
-        obj_in: Union[UpdateSchemaType, Dict[str, Any]]
+            self,
+            db: Session,
+            *,
+            db_obj: ModelType,
+            obj_in: Union[UpdateSchemaType, Dict[str, Any]],
+            req_user: User
     ) -> ModelType:
         obj_data = jsonable_encoder(db_obj)
         if isinstance(obj_in, dict):
@@ -67,8 +74,7 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         db.refresh(db_obj)
         return db_obj
 
-    # FIXME: How to Resolve this? Remove vs Update Conflict
-    def remove(self, db: Session, *, id: int) -> ModelType:
+    def remove(self, db: Session, *, id: int, req_user: User) -> ModelType:
         obj = db.query(self.model).get(id)
         db.delete(obj)
         db.commit()
