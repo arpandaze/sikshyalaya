f81c08b868130557689e3bbaf22a923148a04842
Daze <dazehere@yandex.com>
2021-04-25 16:30:27 +0545

    Duration field added

--- backend/models/class_session.py
+++ backend/models/class_session.py
@@ -16,5 +16,6 @@ class ClassSession(Base):
     course = relationship("Course", backref="session")
     group_id = Column(Integer, ForeignKey("group.id"))
     group = relationship("Group", backref="class_session", uselist=False)
+    duration = Column(Integer)
     description = Column(String(length=128))
     __tablename__ = "class_session"
