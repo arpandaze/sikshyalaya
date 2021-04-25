ab6e2539ace5ba8b88b45e27c8e2377c560c1465
Daze <dazehere@yandex.com>
2021-04-25 16:28:17 +0545

    added function to get permission by name

--- backend/cruds/user_permission.py
+++ backend/cruds/user_permission.py
@@ -1,12 +1,17 @@
 from cruds.base import CRUDBase
 from schemas.user_permission import UserPermissionCreate, UserPermissionUpdate
 from models.user_permission import UserPermission
+from models import User
+from sqlalchemy.orm import Session
 
 
 class CRUDUserPermission(
     CRUDBase[UserPermission, UserPermissionCreate, UserPermissionUpdate]
 ):
-    pass
+    def get_by_name(
+        self, db: Session, *, name: str, req_user:User,
+    ):
+        return db.query(self.model).filter(self.model.name == name).first()
 
 
 crud_user_permission = CRUDUserPermission(UserPermission)
