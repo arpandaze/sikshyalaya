6176814b6da7d90737c09ddaacf7cf01558ad4ec
javaclll <yugu.upadhyaya@gmail.com>
2021-04-28 18:14:03 +0545

    join_year commented since it is not defined in schema, and not sent while creating

--- backend/models/group.py
+++ backend/models/group.py
@@ -3,11 +3,10 @@ from sqlalchemy import Column, Integer, ForeignKey, SmallInteger
 from sqlalchemy.orm import relationship
 
 
-
 class Group(Base):
     id = Column(Integer, primary_key=True)
     program_id = Column(Integer, ForeignKey("program.id"))
     program = relationship("Program", backref="groups")
     sem = Column(SmallInteger)
-    join_year = Column(SmallInteger)
+    # join_year = Column(SmallInteger)
     __tablename__ = "group"
