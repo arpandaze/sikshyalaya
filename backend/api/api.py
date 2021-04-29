184a16b2fbf1c65e5dbcb23b27021500ca6f7e57
javaclll <yugu.upadhyaya@gmail.com>
2021-04-29 19:52:40 +0545

    included group endpoint to api router

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter
 
+from api.endpoints import program, teacher_note, users, group
 from api.endpoints import (
-    program,
-    teacher_note,
-    users,
+    login,
+    utils,
+    course,
+    school,
+    department,
+    class_session,
+    personal_note,
+    user_permission,
 )
-from api.endpoints import login, utils, course, school, department, class_session, personal_note, user_permission
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["Login"])
@@ -21,7 +26,10 @@ api_router.include_router(
     personal_note.router, prefix="/personal_note", tags=["Personal Notes"]
 )
 api_router.include_router(program.router, prefix="/program", tags=["Programs"])
-api_router.include_router(user_permission.router, prefix="/user_permission", tags=["User Permission"])
+api_router.include_router(
+    user_permission.router, prefix="/user_permission", tags=["User Permission"]
+)
 api_router.include_router(
     teacher_note.router, prefix="/teacher_note", tags=["Teacher Notes"]
 )
+api_router.include_router(group.router, prefix="/group", tags=["Group"])
