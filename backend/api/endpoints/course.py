142d31122b6800caa4e0c1e882bbbac247fec7e0
javaclll <yugu.upadhyaya@gmail.com>
2021-04-29 15:45:43 +0545

    course endpoint modified

--- backend/api/endpoints/course.py
+++ backend/api/endpoints/course.py
@@ -6,8 +6,17 @@ from sqlalchemy.orm import Session
 from utils import deps
 from cruds import crud_course
 from schemas.course import Course, CourseCreate, CourseUpdate
+from core import settings
+from models import User
+from fastapi import HTTPException
 
 router = APIRouter()
+
+# Get:
+# All
+
+# Post, Update:
+# Only admin and above
 
 
 @router.get("/course", response_model=List[Course])
@@ -19,9 +28,23 @@ def get_course(
 
 
 @router.post("/course", response_model=Course)
-def create_course(db: Session = Depends(deps.get_db), *, obj_in: CourseCreate) -> Any:
-    course = crud_course.create(db, obj_in=obj_in)
-    return course
+def create_course(
+    db: Session = Depends(deps.get_db),
+    *,
+    obj_in: CourseCreate,
+    current_user: User = Depends(deps.get_current_active_user)
+) -> Any:
+    if current_user:
+        if current_user.user_type > settings.UserType.TEACHER:
+            course = crud_course.create(db, obj_in=obj_in)
+            return course
+        else:
+            raise HTTPException(
+                status_code=401,
+                detail="user has no authorization for creating courses",
+            )
+    else:
+        raise HTTPException(status_code=404, detail="user not found!")
 
 
 @router.get("/course/{id}", response_model=Course)
