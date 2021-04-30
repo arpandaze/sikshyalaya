d80e0b946fcae0f449e1eb0decd4e0e71a423ddf
Daze <dazehere@yandex.com>
2021-04-30 12:35:28 +0545

    get_user_class_session() updated

--- backend/cruds/class_session.py
+++ backend/cruds/class_session.py
@@ -6,15 +6,14 @@ from models import ClassSession
 from models import User
 from schemas import ClassSessionCreate, ClassSessionUpdate
 from sqlalchemy.orm import Session
+from core.config import settings
 
 
 class CRUDClassSession(CRUDBase[ClassSession, ClassSessionCreate, ClassSessionUpdate]):
     def create(self, db: Session, *, obj_in: ClassSessionCreate) -> Any:
         if obj_in.instructors:
-            print(crud_user.get(db=db, id=2))
-            instructors = list(
-                map(lambda id: crud_user.get(db=db, id=id), obj_in.instructors)
-            )
+            instructors = [crud_user.get(db=db, id=id) for id in obj_in.instructors]
+
         else:
             instructors = []
 
@@ -24,14 +23,46 @@ class CRUDClassSession(CRUDBase[ClassSession, ClassSessionCreate, ClassSessionUp
             instructors=instructors,  # noqa
             course_id=obj_in.course_id,  # noqa
             description=obj_in.description,  # noqa
+            duration=obj_in.duration,
         )
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
         return db_obj
 
-    def get_user_class_sessions(self, db: Session, user: User) -> List[ClassSession]:
-        return db.query(self.model).filter(ClassSession.group_id == user.group_id).all()
+    def get_user_class_session(
+        self, db: Session, user: User, id: int = None
+    ) -> List[ClassSession]:
+        if user.user_type == settings.UserType.STUDENT.value:
+            if not id:
+                class_sessions = (
+                    db.query(self.model)
+                    .filter(ClassSession.group_id == user.group_id)
+                    .all()
+                )
+            else:
+                class_sessions = (
+                    db.query(self.model)
+                    .filter(ClassSession.id == id)
+                    .filter(ClassSession.group_id == user.group_id)
+                    .first()
+                )
+        else:  # if user.user_type == settings.UserType.TEACHER.value:
+            if not id:
+                class_sessions = (
+                    db.query(self.model)
+                    .filter(ClassSession.instructor.contains(user))
+                    .all()
+                )
+            else:
+                class_sessions = (
+                    db.query(self.model)
+                    .filter(ClassSession.id == id)
+                    .filter(ClassSession.instructor.contains(user))
+                    .first()
+                )
+
+        return class_sessions
 
 
 crud_class_session = CRUDClassSession(ClassSession)
