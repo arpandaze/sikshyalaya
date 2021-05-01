f307d548a0d200ab79cf4cc83bfd07f0274bc4b9
Daze <dazehere@yandex.com>
2021-05-01 09:09:31 +0545

    Get and Post permission update

--- backend/api/endpoints/class_session.py
+++ backend/api/endpoints/class_session.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 
 from utils import deps
 from cruds import crud_class_session, crud_user
-from schemas import ClassSession, ClassSessionUpdate
+from schemas import ClassSession, ClassSessionUpdate, ClassSessionCreate
 from utils.deps import get_current_active_teacher_or_above, get_current_active_user
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
@@ -20,24 +20,34 @@ router = APIRouter()
 
 @router.get("/", response_model=List[ClassSession])
 def get_class_session(
-    db: Session = Depends(deps.get_db), skip: int = 0, limit: int = 100
+    db: Session = Depends(deps.get_db),
+    user=Depends(get_current_active_user),
+    skip: int = 0,
+    limit: int = 100,
 ) -> Any:
-    user = crud_user.get_by_id(db, id=1)
-    class_sessions = crud_class_session.get_user_class_sessions(db, user=user)
+    class_sessions = crud_class_session.get_user_class_session(db, user=user)
     return class_sessions
 
 
 @router.post("/", response_model=ClassSession)
 def create_class_session(
-    db: Session = Depends(deps.get_db), *, obj_in: ClassSessionUpdate
+    db: Session = Depends(deps.get_db),
+    user=Depends(get_current_active_teacher_or_above),
+    *,
+    obj_in: ClassSessionCreate,
 ) -> Any:
     class_session = crud_class_session.create(db, obj_in=obj_in)
     return class_session
 
 
 @router.get("/{id}", response_model=ClassSession)
-def get_specific_class_session(db: Session = Depends(deps.get_db), *, id: int) -> Any:
-    class_session = crud_class_session.get(db, id)
+def get_specific_class_session(
+    db: Session = Depends(deps.get_db),
+    user=Depends(get_current_active_user),
+    *,
+    id: int,
+) -> Any:
+    class_session = crud_class_session.get_user_class_session(db=db, user=user, id=id)
     return class_session
 
 
@@ -80,7 +90,7 @@ async def create_upload_files(
     return {"msg": "success"}
 
 
-@router.get("{id}/file/{filename}")
+@router.get("{id}/file/{filename}")  # TODO: File caching
 async def get_upload_files(
     db: Session = Depends(deps.get_db),
     req_user=Depends(get_current_active_user),
