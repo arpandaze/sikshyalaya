0ba4216661669e45298fa9075da7e84a4fdd94da
javaclll <yugu.upadhyaya@gmail.com>
2021-05-01 12:52:06 +0545

    populate script changed w.r to changes in user and group

--- backend/utils/populate.py
+++ backend/utils/populate.py
@@ -75,7 +75,11 @@ def populate_program():
 def populate_group():
     for i in range(10):
         try:
-            group = GroupCreate(program_id=randint(1, 10), sem=randint(1, 8))
+            group = GroupCreate(
+                program_id=randint(1, 10),
+                sem=randint(1, 8),
+                course=list(range(randint(1, 9), randint(10, 20))),
+            )
             crud_group.create(db, obj_in=group)
         except Exception as e:  # noqa
             print(e)
@@ -88,13 +92,13 @@ def populate_user():
                 full_name=fake.name(),
                 email=fake.email(),
                 group_id=randint(1, 10),
-                enrolled_course=range(randint(1, 9), randint(10, 20)),
+                teacher_group=list(range(randint(1, 5), randint(6, 10))),
                 dob=fake.date_time(),
                 address=fake.address(),
                 contact_number=fake.phone_number(),
                 password=fake.password(),
                 user_type=randint(1, 3),
-                permission=list(range(randint(1, 9), randint(10, 20)))
+                permission=list(range(randint(1, 9), randint(10, 20))),
             )
             crud_user.create(db, obj_in=user)
         except Exception as e:  # noqa
