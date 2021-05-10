37bba540acab5760915a2530ce3fa74944fc7415
javaclll <yugu.upadhyaya@gmail.com>
2021-05-10 15:44:52 +0545

    CRUDQuizQuestion added for performing crud to question table

--- backend/cruds/quiz.py
+++ backend/cruds/quiz.py
@@ -1,6 +1,6 @@
 from cruds.base import CRUDBase
-from schemas.quiz import QuizCreate, QuizUpdate
-from models.quiz import Quiz
+from schemas.quiz import QuizCreate, QuizUpdate, QuizQuestionCreate, QuizQuestionUpdate
+from models.quiz import Quiz, QuizQuestion
 
 
 class CRUDQuiz(CRUDBase[Quiz, QuizCreate, QuizUpdate]):
@@ -8,3 +8,10 @@ class CRUDQuiz(CRUDBase[Quiz, QuizCreate, QuizUpdate]):
 
 
 crud_quiz = CRUDQuiz(Quiz)
+
+
+class CRUDQuizQuestion(CRUDBase[QuizQuestion, QuizQuestionCreate, QuizQuestionUpdate]):
+    pass
+
+
+crud_question = CRUDQuizQuestion(QuizQuestion)
