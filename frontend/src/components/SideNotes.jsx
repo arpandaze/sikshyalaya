644019aa25932fbbdefe210bb22594a5f7cc3651
Rushab11 <humagainrushab@gmail.com>
2021-05-10 14:56:45 +0545

    Media Query added for SideNotes component

--- frontend/src/components/SideNotes.jsx
+++ frontend/src/components/SideNotes.jsx
@@ -3,30 +3,30 @@ import Grid from "@material-ui/core/Grid";
 import "./statics/css/sideNotes.css";
 
 const SideNotes = ({ title, content, onClick, ...rest }) => {
-  return (
-    <Grid
-      container
-      direction="column"
-      className="sideNotes_root"
-      onClick={onClick}
-    >
-      <Grid item className="sideNotes_innerContainer">
+    return (
         <Grid
-          container
-          direction="column"
-          alignItems="flex-start"
-          wrap="nowrap"
+            container
+            direction="column"
+            className="sideNotes_root"
+            onClick={onClick}
         >
-          <Grid item className="sideNotes_titleTextContainer">
-            <p className="sideNotes_titleText">{title}</p>
-          </Grid>
-          <Grid item className="sideNotes_contentTextContainer">
-            <p className="sideNotes_contentText">{content}</p>
-          </Grid>
+            <Grid item className="sideNotes_innerContainer">
+                <Grid
+                    container
+                    direction="column"
+                    alignItems="center"
+                    wrap="nowrap"
+                >
+                    <Grid item className="sideNotes_titleTextContainer">
+                        <p className="sideNotes_titleText">{title}</p>
+                    </Grid>
+                    <Grid item className="sideNotes_contentTextContainer">
+                        <p className="sideNotes_contentText">{content}</p>
+                    </Grid>
+                </Grid>
+            </Grid>
         </Grid>
-      </Grid>
-    </Grid>
-  );
+    );
 };
 
 export default SideNotes;
