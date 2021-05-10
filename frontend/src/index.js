5365d67f1a0d737755523fde8631b3bc2990d410
waterlulu12 <aatishshrestha123@gmail.com>
2021-05-10 10:08:39 +0545

    Refactor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ import { Helmet } from "react-helmet";
 ReactDOM.render(
   <BrowserRouter>
     <Helmet>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="initial-scale=1.0" />
     </Helmet>
     <App />
   </BrowserRouter>,
