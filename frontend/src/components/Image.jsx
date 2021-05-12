0fa8536139a0b34fa1f7ce7f11d3cf7ddfb28765
Rushab11 <humagainrushab@gmail.com>
2021-05-12 17:22:38 +0545

    Fixed images

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./statics/css/image.css";
 
 const Image = ({ src, alt, addStyles, ...rest }) => {
-  return <img src={src} alt={alt} className={addStyles} {...rest} />;
+  return (
+    <img src={src} alt={alt} className={"main_image " + addStyles} {...rest} />
+  );
 };
 
 export default Image;
