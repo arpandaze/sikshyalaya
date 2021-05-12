2c6f220ebb8cbabe7ac55954f91277686cf7cdee
Rushab11 <humagainrushab@gmail.com>
2021-05-12 16:19:11 +0545

    Fixed minor issue

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import "./statics/css/image.css";
 
 const Image = ({ src, alt, addStyles, ...rest }) => {
-  return (
-    <img {...rest} src={src} alt={alt} className={"main_image " + addStyles} />
-  );
+  return <img src={src} alt={alt} className={addStyles} {...rest} />;
 };
 
 export default Image;
