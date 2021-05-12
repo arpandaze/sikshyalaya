efc7c6f1947e3cca651f39c8e885f900ac056f04
Rushab11 <humagainrushab@gmail.com>
2021-05-12 16:25:19 +0545

    Login Component Fixed

--- frontend/src/views/Login/Login.jsx
+++ frontend/src/views/Login/Login.jsx
@@ -7,52 +7,37 @@ import "./statics/css/login.css";
 const Login = (props) => {
   const [active, setActive] = useState(0);
   return (
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      className="login_root"
-    >
-      <Grid item xs={4}>
-        <Grid
-          container
-          direction="column"
-          justify="flex-start"
-          alignItems="center"
-          spacing={0}
-        >
-          <Grid item className="login_logoBox">
-            <Image src={logo} alt={{ logo }} />
-          </Grid>
-          <Grid container item className="login_copyrightContainer">
-            <p className="login_copyrightText">©2021 Sikshyalaya</p>
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs={4}>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-          className="login_loginBoxContainer"
-        >
-          <Grid item className="login_loginBox">
-            <Grid
-              container
-              spacing={4}
-              direction="row"
-              justify="center"
-              alignItems="center"
-            >
-              {props.children}
+    <>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        alignContent="center"
+        className="login_root"
+      >
+        <Grid item className="login_logoBox">
+          <Grid container direction="row" alignItems="flex-start">
+            <Grid item className="login_logo">
+              <Image src={logo} alt="Sikshyalaya" addStyles="login_image" />
             </Grid>
           </Grid>
         </Grid>
+        <Grid item>
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            justify="center"
+            className="login_loginBoxContainer"
+          >
+            <Grid item>{props.children}</Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <p className="login_copyrightText">©2021 Sikshyalaya</p>
+        </Grid>
       </Grid>
-      <Grid item xs={4}></Grid>
-    </Grid>
+    </>
   );
 };
 
