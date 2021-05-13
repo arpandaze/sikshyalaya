db205cd150b78aee7afd6804fe47f87bea57a419
Daze <dazehere@yandex.com>
2021-05-13 09:22:37 +0545

    Login integration test

--- frontend/src/views/Login/LoginBox.jsx
+++ frontend/src/views/Login/LoginBox.jsx
@@ -5,123 +5,155 @@ import * as yup from "yup";
 import Grid from "@material-ui/core/Grid";
 import Login from "./Login";
 import "./statics/css/loginCommon.css";
+import axios from "axios";
+import Checkbox from "../../components/Checkbox";
+import configs from "../../utils/configs";
 
 const validationSchema = yup.object({
-  email: yup
-    .string("Enter your email")
-    .email("Enter a valid email")
-    .required("Email is required"),
-  password: yup
-    .string("Enter your password")
-    .min(4, "Minimum 4 characters")
-    .required("Password is required"),
+    email: yup
+        .string("Enter your email")
+        .email("Enter a valid email")
+        .required("Email is required"),
+    password: yup
+        .string("Enter your password")
+        .min(4, "Minimum 4 characters")
+        .required("Password is required"),
 });
 const StudentLoginBox = () => {
-  return (
-    <Login>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        className="loginCommon_loginBoxContainer"
-      >
-        <Grid item>
-          <h1 className="loginCommon_label">Login</h1>
-        </Grid>
-        <Grid item>
-          <Formik
-            initialValues={{
-              email: "",
-              password: "",
-            }}
-            validationSchema={validationSchema}
-            //   onSubmit={async (values) => {
-            //     await new Promise((r) => setTimeout(r, 500));
-            //     alert(JSON.stringify(values, null, 2));
-            //   }}
-          >
-            <Form>
-              <Grid
+    const onSubmit = (values) => {
+        let data = {
+            username: values.email,
+            password: values.password,
+            remember_me: values.remember_me,
+        };
+        axios
+            .post(`${configs.HOST}/api/v1/auth/web`, data, {
+                withCredentials: true,
+            })
+            .then((response) => {
+                if (
+                    !(
+                        response.status === 200 &&
+                        response.data.status === "success"
+                    )
+                ) {
+                    throw "Login failed!";
+                } else {
+                    document.location = "/landing";
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    return (
+        <Login>
+            <Grid
                 container
-                spacing={5}
-                direction="column"
-                alignItems="flex-start"
-              >
-                <Grid item spacing={10}>
-                  <Field
-                    id="email"
-                    name="email"
-                    placeholder="Email"
-                    className="loginCommon_inputButton"
-                  />
-                </Grid>
-
-                <Grid item spacing={10}>
-                  <Field
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                    className="loginCommon_inputButton"
-                  />
-                </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  className="loginCommon_rememberMeCheckContainer"
-                >
-                  <input
-                    type="checkbox"
-                    value="lsRememberMe"
-                    id="rememberMe"
-                    className="loginCommon_rememberMeCheck"
-                  />
-                  <label for="rememberMe">Remember me</label>
-                </Grid>
-              </Grid>
-              <Grid
-                container
-                spacing={5}
                 direction="column"
                 justify="center"
                 alignItems="center"
-              >
-                <Grid item spacing={10}>
-                  <Button name="Login" addStyles="loginCommon_loginButton" />
+                className="loginCommon_loginBoxContainer"
+            >
+                <Grid item>
+                    <h1 className="loginCommon_label">Login</h1>
                 </Grid>
-                <Grid item spacing={10}>
-                  <Grid
-                    container
-                    spacing={2}
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                  >
-                    <Grid item>
-                      <div className="loginCommon_line"></div>
-                    </Grid>
-                    <Grid item>
-                      <p style={{ fontSize: "1.2em" }}>or</p>
-                    </Grid>
-                    <Grid item>
-                      <div className="loginCommon_line"></div>
-                    </Grid>
-                  </Grid>
+                <Grid item>
+                    <Formik
+                        initialValues={{
+                            email: "",
+                            password: "",
+                            remember_me: false,
+                        }}
+                        validationSchema={validationSchema}
+                        onSubmit={onSubmit}
+                    >
+                        <Form>
+                            <Grid
+                                container
+                                spacing={5}
+                                direction="column"
+                                alignItems="flex-start"
+                            >
+                                <Grid item spacing={10}>
+                                    <Field
+                                        id="email"
+                                        name="email"
+                                        placeholder="Email"
+                                        className="loginCommon_inputButton"
+                                    />
+                                </Grid>
+
+                                <Grid item spacing={10}>
+                                    <Field
+                                        type="password"
+                                        id="password"
+                                        name="password"
+                                        placeholder="Password"
+                                        className="loginCommon_inputButton"
+                                    />
+                                </Grid>
+                                <Grid
+                                    item
+                                    xs={12}
+                                    className="loginCommon_rememberMeCheckContainer"
+                                >
+                                    <Checkbox
+                                        name="remember_me"
+                                        value="remember_me"
+                                    />
+                                    <label for="rememberMe">Remember me</label>
+                                </Grid>
+                            </Grid>
+                            <Grid
+                                container
+                                spacing={5}
+                                direction="column"
+                                justify="center"
+                                alignItems="center"
+                            >
+                                <Grid item spacing={10}>
+                                    <Button
+                                        name="Login"
+                                        type="submit"
+                                        addStyles="loginCommon_loginButton"
+                                    />
+                                </Grid>
+                                <Grid item spacing={10}>
+                                    <Grid
+                                        container
+                                        spacing={2}
+                                        direction="row"
+                                        justify="center"
+                                        alignItems="center"
+                                    >
+                                        <Grid item>
+                                            <div className="loginCommon_line"></div>
+                                        </Grid>
+                                        <Grid item>
+                                            <p style={{ fontSize: "1.2em" }}>
+                                                or
+                                            </p>
+                                        </Grid>
+                                        <Grid item>
+                                            <div className="loginCommon_line"></div>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <Grid item spacing={10}>
+                                    <Button
+                                        type="button"
+                                        name="Conitnue as Guest"
+                                        addStyles="loginCommon_guestButton"
+                                    />
+                                </Grid>
+                            </Grid>
+                        </Form>
+                    </Formik>
                 </Grid>
-                <Grid item spacing={10}>
-                  <Button
-                    name="Conitnue as Guest"
-                    addStyles="loginCommon_guestButton"
-                  />
-                </Grid>
-              </Grid>
-            </Form>
-          </Formik>
-        </Grid>
-      </Grid>
-    </Login>
-  );
+            </Grid>
+        </Login>
+    );
 };
 
 export default StudentLoginBox;
