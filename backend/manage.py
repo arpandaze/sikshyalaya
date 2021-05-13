5f35f68515441f0592dd01bb918dd80cc74153af
Daze <dazehere@yandex.com>
2021-05-13 09:17:54 +0545

    minor changes

--- backend/manage.py
+++ backend/manage.py
@@ -55,7 +55,6 @@ if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        # allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
