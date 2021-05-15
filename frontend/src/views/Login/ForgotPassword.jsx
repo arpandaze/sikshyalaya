c0a637d71f52ebbde16de03ac98f6ddaf318443c
Daze <dazehere@yandex.com>
2021-05-15 21:55:55 +0545

    Password reset implemented

--- frontend/src/views/Login/ForgotPassword.jsx
+++ frontend/src/views/Login/ForgotPassword.jsx
@@ -5,9 +5,8 @@ import * as yup from "yup";
 import Grid from "@material-ui/core/Grid";
 import Login from "./Login";
 import "./statics/css/forgotPassword.css";
-import axios from "axios";
 import configs from "../../utils/configs";
-
+import { postReq } from "../../utils/API";
 
 const validationSchema = yup.object({
     email: yup
@@ -16,91 +15,74 @@ const validationSchema = yup.object({
         .required("Email is required"),
 });
 
-
-const ForgotPassword = () => {
-    const onSubmit = (values) => {
-    
-    let email=values.email
-    axios
-    .post(`${configs.API_HOST}/api/v1/auth/password-recovery/${email}`, {
-        withCredentials: true,
-    })
-    .then((response) => {
-       
-        if (
-            !(
-                response.status === 200 &&
-                response.data.msg === "Password recovery email sent"
-            )
-        ) {
-            throw "Email Not Sent!";
-        } else {
-            document.location = "/landing"; // should show a page to suggest go and check mail
+const ForgotPassword = ({ setResetState }) => {
+    const onSubmit = async (values) => {
+        let data = {
+            email: values.email
         }
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-  };
+        let resp = await postReq(
+            "/api/v1/auth/password-recovery",
+            null,
+            data
+        );
+        if (resp.status == 200) {
+            setResetState(4);
+        }
+    };
+
     return (
-        <Login>
-            <Grid
-                container
-                direction="column"
-                justify="center"
-                alignItems="center"
-                className="forgotPassword_BoxContainer"
-            >
-                <Grid item>
-                    <h1 className="forgotPassword_label">Confirm Email</h1>
-                </Grid>
-                <Grid item>
-                    <Formik
-                        initialValues={{
-                            email: "",
-                        }}
-                        validationSchema={validationSchema}
-                        onSubmit={onSubmit}
-                        //   onSubmit={async (values) => {
-                        //     await new Promise((r) => setTimeout(r, 500));
-                        //     alert(JSON.stringify(values, null, 2));
-                        //   }}
-                    >
-                        <Form>
-                            <Grid
-                                container
-                                spacing={5}
-                                direction="column"
-                                alignItems="flex-start"
-                            >
-                                <Grid item spacing={10}>
-                                    <Field
-                                        id="email"
-                                        name="email"
-                                        placeholder="Enter your Email"
-                                        className="forgotPassword_inputButton"
-                                    />
-                                </Grid>
-                            </Grid>
-                            <Grid
-                                container
-                                direction="column"
-                                justify="center"
-                                alignItems="center"
-                            >
-                                <Grid item xs={12}>
-                                    <Button
-                                        type="submit"
-                                        name="Confirm your Email"
-                                        addStyles="forgotPassword_Button"
-                                    />
-                                </Grid>
-                            </Grid>
-                        </Form>
-                    </Formik>
-                </Grid>
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            className="forgotPassword_BoxContainer"
+        >
+            <Grid item>
+                <h1 className="forgotPassword_label">Confirm Email</h1>
             </Grid>
-        </Login>
+            <Grid item>
+                <Formik
+                    initialValues={{
+                        email: "",
+                    }}
+                    validationSchema={validationSchema}
+                    onSubmit={onSubmit}
+                >
+                    <Form>
+                        <Grid
+                            container
+                            spacing={5}
+                            direction="column"
+                            alignItems="flex-start"
+                        >
+                            <Grid item spacing={10}>
+                                <Field
+                                    id="email"
+                                    name="email"
+                                    placeholder="Enter your Email"
+                                    className="forgotPassword_inputButton"
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid
+                            container
+                            direction="column"
+                            justify="center"
+                            alignItems="center"
+                        >
+                            <Grid item xs={12}>
+                                <Button
+                                    type="submit"
+                                    name="Confirm your Email"
+                                    addStyles="forgotPassword_Button"
+                                />
+                            </Grid>
+                        </Grid>
+                    </Form>
+                </Formik>
+            </Grid>
+        </Grid>
     );
 };
 
--- frontend/src/views/Login/ResetPassword.jsx
+++ frontend/src/views/Login/ResetPassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Formik, Field, Form } from "formik";
 import Button from "../../components/Button";
 import * as yup from "yup";
@@ -6,163 +6,200 @@ import Grid from "@material-ui/core/Grid";
 import logo from "../../assets/logo.png";
 import axios from "axios";
 import configs from "../../utils/configs";
-import {get, set} from "idb-keyval";
+import { get, set } from "idb-keyval";
 import Image from "../../components/Image";
 import Login from "./Login";
 import "./statics/css/reset.css";
+import { postReq } from "../../utils/API";
+import { Redirect, useHistory } from "react-router-dom";
+import ForgotPassword from "./ForgotPassword";
 
 const validationSchema = yup.object({
-  password: yup
-    .string("Enter your password")
-    .min(4, "Minimum 4 characters")
-    .required("Password is required"),
-  confirm_password: yup
-    .string()
-    .when("password", {
-    is: val => (val && val.length > 0 ? true : false),
-    then: yup.string().oneOf(
-      [yup.ref("password")],
-      "Both password need to be the same"
-    )
-    })
+    password: yup
+        .string("Enter your password")
+        .min(4, "Minimum 4 characters")
+        .required("Password is required"),
+    confirm_password: yup.string().when("password", {
+        is: (val) => (val && val.length > 0 ? true : false),
+        then: yup
+            .string()
+            .oneOf([yup.ref("password")], "Both password need to be the same"),
+    }),
 });
-const ResetPassword = () => {
-  const onSubmit = (values) => {
-    
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    
-    let token = urlParams.get('token')
-    
-    if (token === null){
-      token = ''
-    }
 
-    let data = {
-        token: token,
-        new_password: values.password, 
+const ResetPassword = () => {
+    const states = {
+        error: 1,
+        reset_success: 2,
+        send_email: 3,
+        reset_email_success: 4,
     };
 
-    axios
-        .post(`${configs.API_HOST}/api/v1/auth/reset-password`, data, {
-            withCredentials: true,
-        })
-        .then((response) => {
-           
-            if (
-                !(
-                    response.status === 200 &&
-                    response.data.msg === "Password updated successfully"
-                )
-            ) {
-                throw "Password Reset Failed!";
-            } else {
+    const history = useHistory();
 
-                document.location = "/landing";
+    const [resetState, setResetState] = useState(null);
+
+    useEffect(() => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+
+        let token = urlParams.get("token");
+
+        if (token === null) {
+            setResetState(states.send_email);
+        }
+    }, []);
+
+    const onEmailSubmit = async (values) => {
+        let resp = await postReq(
+            "/api/v1/auth/password-recovery",
+            values.email
+        );
+    };
+
+    const onPasswordsSubmit = async (values) => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+
+        let token = urlParams.get("token");
+
+        if (token === null) {
+            setResetState(states.error);
+        } else {
+            let data = {
+                token: token,
+                new_password: values.password,
+            };
+
+            let resp = await postReq("/api/v1/auth/reset-password", data);
+            console.log(resp);
+
+            if (resp.status === 200) {
+                setResetState(states.success);
             }
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-  };
-  return (
-    <Login>
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        className="resetCommon_resetBoxContainer"
-      >
-        <Grid item>
-          <h1 className="resetCommon_label">Reset Password</h1>
-        </Grid>
-        <Grid item>
-          <Formik
-            initialValues={{
-              password: "",
-              confirm_password: "",
-            }}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
-          >
-            <Form>
-              <Grid
+        }
+    };
+    return (
+        <Login>
+            <Grid
                 container
-                spacing={5}
-                direction="column"
-                alignItems="flex-start"
-              >
-                <Grid item spacing={10}>
-                  <Field
-                    id="password"
-                    name="password"
-                    placeholder="Password"
-                    className="resetCommon_inputButton"
-                  />
-                </Grid>
-
-                <Grid item spacing={10}>
-                  <Field
-                    type="password"
-                    id="confirm_password"
-                    name="confirm_password"
-                    placeholder="Confirm Password"
-                    className="resetCommon_inputButton"
-                  />
-                </Grid>
-              </Grid>
-              <Grid
-                container
-                spacing={5}
                 direction="column"
                 justify="center"
                 alignItems="center"
-              >
-                <Grid item>
-                  <Button type="submit" name="Reset" addStyles="resetCommon_resetButton" />
-                </Grid>
-                <Grid item>
-                  <Grid
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                  >
-                    <Grid item>
-                      <div className="resetCommon_line"></div>
-                    </Grid>
-                    <Grid item>
-                      <p
-                        style={{
-                          margin: "0px",
-                          padding: "0px",
-                          fontSize: "1.2em",
-                        }}
-                      >
-                        or
-                      </p>
-                    </Grid>
-                    <Grid item>
-                      <div className="resetCommon_line"></div>
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <Grid item>
-                  <Button
-                    type="button"
-                    onClick={()=>{console.log("Back to Login")}}
-                    name="Back to Login"
-                    addStyles="resetCommon_guestButton"
-                  />
-                </Grid>
-              </Grid>
-            </Form>
-          </Formik>
-        </Grid>
-      </Grid>
-    </Login>
-  );
+                className="resetCommon_resetBoxContainer"
+            >
+                {resetState == null ? (
+                    <>
+                        <Grid item>
+                            <h1 className="resetCommon_label">
+                                Reset Password
+                            </h1>
+                        </Grid>
+                        <Grid item>
+                            <Formik
+                                initialValues={{
+                                    password: "",
+                                    confirm_password: "",
+                                }}
+                                validationSchema={validationSchema}
+                                onSubmit={onPasswordsSubmit}
+                            >
+                                <Form>
+                                    <Grid
+                                        container
+                                        spacing={5}
+                                        direction="column"
+                                        alignItems="flex-start"
+                                    >
+                                        <Grid item spacing={10}>
+                                            <Field
+                                                type="password"
+                                                id="password"
+                                                name="password"
+                                                placeholder="Password"
+                                                className="resetCommon_inputButton"
+                                            />
+                                        </Grid>
+
+                                        <Grid item spacing={10}>
+                                            <Field
+                                                type="password"
+                                                id="confirm_password"
+                                                name="confirm_password"
+                                                placeholder="Confirm Password"
+                                                className="resetCommon_inputButton"
+                                            />
+                                        </Grid>
+                                    </Grid>
+                                    <Grid
+                                        container
+                                        spacing={5}
+                                        direction="column"
+                                        justify="center"
+                                        alignItems="center"
+                                    >
+                                        <Grid item>
+                                            <Button
+                                                type="submit"
+                                                name="Reset"
+                                                addStyles="resetCommon_resetButton"
+                                            />
+                                        </Grid>
+                                        <Grid item>
+                                            <Grid
+                                                container
+                                                direction="row"
+                                                justify="center"
+                                                alignItems="center"
+                                            >
+                                                <Grid item>
+                                                    <div className="resetCommon_line"></div>
+                                                </Grid>
+                                                <Grid item>
+                                                    <p
+                                                        style={{
+                                                            margin: "0px",
+                                                            padding: "0px",
+                                                            fontSize: "1.2em",
+                                                        }}
+                                                    >
+                                                        or
+                                                    </p>
+                                                </Grid>
+                                                <Grid item>
+                                                    <div className="resetCommon_line"></div>
+                                                </Grid>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid item>
+                                            <Button
+                                                type="button"
+                                                onClick={() => {
+                                                    history.push("/login");
+                                                }}
+                                                name="Back to Login"
+                                                addStyles="resetCommon_guestButton"
+                                            />
+                                        </Grid>
+                                    </Grid>
+                                </Form>
+                            </Formik>
+                        </Grid>
+                    </>
+                ) : resetState == states.reset_success ? (
+                    <h1>Password has been reset successfully!</h1>
+                ) : resetState == states.reset_email_success ? (
+                    <h1>Verification email has been sent!</h1>
+                ) : resetState == states.send_email ? (
+                    <ForgotPassword
+                        setResetState={(state) => setResetState(state)}
+                    />
+                ) : (
+                    <h1>Valid token not found!</h1>
+                )}
+            </Grid>
+        </Login>
+    );
 };
 
 export default ResetPassword;
