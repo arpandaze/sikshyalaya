76c874021dd0659a7f7aae42fead9f05b6f0b824
Rushab11 <humagainrushab@gmail.com>
2021-05-16 15:42:04 +0545

    Forgot password added

--- frontend/src/views/Login/Login.jsx
+++ frontend/src/views/Login/Login.jsx
@@ -3,47 +3,45 @@ import Grid from "@material-ui/core/Grid";
 import logo from "../../assets/logo.png";
 import Image from "../../components/Image";
 import "./statics/css/login.css";
+import { Link } from "react-router-dom";
 import { UserContext } from "../../utils/UserContext";
 import { Redirect } from "react-router-dom";
 import configs from "../../utils/configs";
+import reactJoiValidation from "react-joi-validation";
 
 const Login = (props) => {
-    const [active, setActive] = useState(0);
-    return (
-        <Grid
-            container
-            direction="column"
-            alignItems="center"
-            alignContent="center"
-            className="login_root"
-        >
-            <Grid item className="login_logoBox">
-                <Grid container direction="row" alignItems="flex-start">
-                    <Grid item className="login_logo">
-                        <Image
-                            src={logo}
-                            alt="Sikshyalaya"
-                            addStyles="login_image"
-                        />
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid item>
-                <Grid
-                    container
-                    direction="row"
-                    alignItems="center"
-                    justify="center"
-                    className="login_loginBoxContainer"
-                >
-                    <Grid item>{props.children}</Grid>
-                </Grid>
-            </Grid>
-            <Grid item>
-                <p className="login_copyrightText">©2021 Sikshyalaya</p>
-            </Grid>
+  const [active, setActive] = useState(0);
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      alignContent="center"
+      className="login_root"
+      wrap="nowrap"
+    >
+      <Grid item className="login_logoBox">
+        <Grid container direction="row" alignItems="flex-start">
+          <Grid item className="login_logo">
+            <Link to="/login">
+              <Image src={logo} alt="Sikshyalaya" addStyles="login_image" />
+            </Link>
+          </Grid>
         </Grid>
-    );
+      </Grid>
+      <Grid item>
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          justify="center"
+          className="login_loginBoxContainer"
+        >
+          <Grid item>{props.children}</Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Login;
