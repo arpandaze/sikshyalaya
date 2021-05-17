7d086a32a322d4ac36fb7f59b7d1b4ade13c7855
javaclll <yugu.upadhyaya@gmail.com>
2021-05-18 04:28:31 +0545

    program population data added

---
+++ backend/utils/populationdata/program.py
@@ -0,0 +1,78 @@
+from typing import List
+from pydantic import Json
+
+programs: List[Json] = [
+    {
+        "name": "Bachelors of Science in Computer Science",
+        "department_id": 1
+    },
+    {
+        "name": "Bachelors of Engineering in Computer Engineering",
+        "department_id": 1
+    },
+    {
+        "name": "Bachelors of Engineering in Mechanical Engineering",
+        "department_id": 2
+    },
+
+    {
+        "name": "Bachelors of Science in Enivironmental Science",
+        "department_id": 3
+    },
+    {
+        "name": "Bachelors of Technology in Environmental Engineering",
+        "department_id": 3
+    },
+    {
+        "name": "Bachelors of Business in Information System",
+        "department_id": 4
+    },
+    {
+        "name": "Bachelors of Business Administration",
+        "department_id": 4
+    },
+    {
+        "name": "Bachelors of Legislative Law",
+        "department_id": 5
+    },
+    {
+        "name": "Bachelors of Medicine, Bachelors of Surgery",
+        "department_id": 6
+    },
+    {
+        "name": "Bachelors of Dental Surgery",
+        "department_id": 6
+    },
+    {
+        "name": "Bachelors of Science in Nursing",
+        "department_id": 6
+    },
+    {
+        "name": "Bachelors of Music Education",
+        "department_id": 7
+    },
+    {
+        "name": "Bachelors of Arts in Music",
+        "department_id": 7
+    },
+    {
+        "name": "Bachelors of Fine Arts",
+        "department_id": 8
+    },
+    {
+        "name": "Bachelors of Arts in Culture and Arts",
+        "department_id": 8
+    },
+    {
+        "name": "Bachelors of Arts in Photography",
+        "department_id": 8
+    },
+    {
+        "name": "Bachelors of Education Studies",
+        "department_id": 9
+    },
+    {
+        "name": "Bachelors of Education in English Literature",
+        "department_id": 9
+    },
+]
