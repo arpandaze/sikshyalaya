dcea0be90e48a878c97ff6133fc5c53a4c1ef793
javaclll <yugu.upadhyaya@gmail.com>
2021-05-18 04:29:13 +0545

    department population data added

---
+++ backend/utils/populationdata/department.py
@@ -0,0 +1,41 @@
+from typing import List
+from pydantic import Json
+
+departments: List[Json] = [
+    {
+        "name": "Department of Computer Science and Engineering",
+        "school_id": 1,
+    },
+    {
+        "name": "Department of Mechanical Engineering",
+        "school_id": 2,
+    },
+    {
+        "name": "Department of Environmental Science and Engineering",
+        "school_id": 1,
+    },
+    {
+        "name": "Department of Management",
+        "school_id": 5,
+    },
+    {
+        "name": "Department of Law",
+        "school_id": 4,
+    },
+    {
+        "name": "Department of Medical Sciences",
+        "school_id": 3,
+    },
+    {
+        "name": "Department of Music",
+        "school_id": 6,
+    },
+    {
+        "name": "Department of Fine Arts",
+        "school_id": 6,
+    },
+    {
+        "name": "Department of Education",
+        "school_id": 7,
+    },
+]
