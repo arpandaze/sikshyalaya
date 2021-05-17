d9ffdce4768c2163df49cfb1e58943d5956911b2
javaclll <yugu.upadhyaya@gmail.com>
2021-05-18 04:28:47 +0545

    personal Note population data added

---
+++ backend/utils/populationdata/personal_note.py
@@ -0,0 +1,125 @@
+from typing import List
+from pydantic import Json
+
+personalNotes: List[Json] = [
+    {
+        "user_id": 3,
+        "course_id": 1,
+        "message":
+        '''
+        A computer three is a programmable machine designed to perform arithmetic and logical operations automatically and sequentially on the input given by the user and gives the desired output after processing. Computer components are divided into two major categories namely hardware and software. Hardware is the machine itself and its connected devices such as monitor, keyboard, mouse etc. Software are the set of programs that make use of hardware for performing various functions.
+        '''
+    },
+    {
+        "user_id": 3,
+        "course_id": 3,
+        "message":
+        '''
+        Environmental engineering three is a job type that is a professional engineering discipline and takes from broad scientific topics like chemistry, biology, ecology, geology, hydraulics, hydrology, microbiology, and mathematics to create solutions that will protect and also improve the health of living organisms and improve the quality of the environment.
+        '''
+    },
+    {
+        "user_id": 4,
+        "course_id": 2,
+        "message":
+        '''
+        Complexity is four usually best measured in a different way. First, in order to not be bound to a particular programming language or machine architecture, it is better to measure the efficiency of the algorithm rather than that of its implementation. For this to be possible, however, the algorithm has to be described in a way which very much looks like the program to be implemented, which is why algorithms are usually best expressed in a form of pseudocode that comes close to the implementation language.
+        '''
+    },
+    {
+        "user_id": 5,
+        "course_id": 1,
+        "message":
+        '''
+        A computer five is a programmable machine designed to perform arithmetic and logical operations automatically and sequentially on the input given by the user and gives the desired output after processing. Computer components are divided into two major categories namely hardware and software. Hardware is the machine itself and its connected devices such as monitor, keyboard, mouse etc. Software are the set of programs that make use of hardware for performing various functions.
+        '''
+    },
+    {
+        "user_id": 6,
+        "course_id": 5,
+        "message":
+        '''
+        Mechanical engineering six emerged as a field during the Industrial Revolution in Europe in the 18th century; however, its development can be traced back several thousand years around the world. In the 19th century, developments in physics led to the development of mechanical engineering science. The field has continually evolved to incorporate advancements; today mechanical engineers are pursuing developments in such areas as composites, mechatronics, and nanotechnology. It also overlaps with aerospace engineering, metallurgical engineering, civil engineering, electrical engineering, manufacturing engineering, chemical engineering, industrial engineering, and other engineering disciplines to varying amounts. Mechanical engineers may also work in the field of biomedical engineering, specifically with biomechanics, transport phenomena, biomechatronics, bionanotechnology, and modelling of biological systems.
+        '''
+    },
+    {
+        "user_id": 7,
+        "course_id": 1,
+        "message":
+        '''
+        A computer seven is a programmable machine designed to perform arithmetic and logical operations automatically and sequentially on the input given by the user and gives the desired output after processing. Computer components are divided into two major categories namely hardware and software. Hardware is the machine itself and its connected devices such as monitor, keyboard, mouse etc. Software are the set of programs that make use of hardware for performing various functions.
+        '''
+    },
+    {
+        "user_id": 8,
+        "course_id": 3,
+        "message":
+        '''
+        Environmental engineering eight is a job type that is a professional engineering discipline and takes from broad scientific topics like chemistry, biology, ecology, geology, hydraulics, hydrology, microbiology, and mathematics to create solutions that will protect and also improve the health of living organisms and improve the quality of the environment.
+        '''
+    },
+    {
+        "user_id": 9,
+        "course_id": 1,
+        "message":
+        '''
+        A computer nine is a programmable machine designed to perform arithmetic and logical operations automatically and sequentially on the input given by the user and gives the desired output after processing. Computer components are divided into two major categories namely hardware and software. Hardware is the machine itself and its connected devices such as monitor, keyboard, mouse etc. Software are the set of programs that make use of hardware for performing various functions.
+        '''
+    },
+    {
+        "user_id": 10,
+        "course_id": 4,
+        "message":
+        '''
+        Environmental engineers ten devise solutions for wastewater management, water and air pollution control, recycling, waste disposal, and public health.[2][3] They design municipal water supply and industrial wastewater treatment systems, and design plans to prevent waterborne diseases and improve sanitation in urban, rural and recreational areas. They evaluate hazardous-waste management systems to evaluate the severity of such hazards, advise on treatment and containment, and develop regulations to prevent mishaps. They implement environmental engineering law, as in assessing the environmental impact of proposed construction projects.
+        '''
+    },
+    {
+        "user_id": 4,
+        "course_id": 4,
+        "message":
+        '''
+        Environmental engineers four devise solutions for wastewater management, water and air pollution control, recycling, waste disposal, and public health.[2][3] They design municipal water supply and industrial wastewater treatment systems, and design plans to prevent waterborne diseases and improve sanitation in urban, rural and recreational areas. They evaluate hazardous-waste management systems to evaluate the severity of such hazards, advise on treatment and containment, and develop regulations to prevent mishaps. They implement environmental engineering law, as in assessing the environmental impact of proposed construction projects.
+        '''
+    },
+    {
+        "user_id": 5,
+        "course_id": 3,
+        "message":
+        '''
+        Environmental engineering five is a job type that is a professional engineering discipline and takes from broad scientific topics like chemistry, biology, ecology, geology, hydraulics, hydrology, microbiology, and mathematics to create solutions that will protect and also improve the health of living organisms and improve the quality of the environment.
+        '''
+    },
+    {
+        "user_id": 7,
+        "course_id": 3,
+        "message":
+        '''
+        Environmental engineering seven is a job type that is a professional engineering discipline and takes from broad scientific topics like chemistry, biology, ecology, geology, hydraulics, hydrology, microbiology, and mathematics to create solutions that will protect and also improve the health of living organisms and improve the quality of the environment.
+        '''
+    },
+    {
+        "user_id": 8,
+        "course_id": 2,
+        "message":
+        '''
+        Complexity is eight usually best measured in a different way. First, in order to not be bound to a particular programming language or machine architecture, it is better to measure the efficiency of the algorithm rather than that of its implementation. For this to be possible, however, the algorithm has to be described in a way which very much looks like the program to be implemented, which is why algorithms are usually best expressed in a form of pseudocode that comes close to the implementation language.
+        '''
+    },
+    {
+        "user_id": 3,
+        "course_id": 3,
+        "message":
+        '''
+        Environmental engineering three is a job type that is a professional engineering discipline and takes from broad scientific topics like chemistry, biology, ecology, geology, hydraulics, hydrology, microbiology, and mathematics to create solutions that will protect and also improve the health of living organisms and improve the quality of the environment.
+        '''
+    },
+    {
+        "user_id": 9,
+        "course_id": 3,
+        "message":
+        '''
+        Environmental engineering nine is a job type that is a professional engineering discipline and takes from broad scientific topics like chemistry, biology, ecology, geology, hydraulics, hydrology, microbiology, and mathematics to create solutions that will protect and also improve the health of living organisms and improve the quality of the environment.
+        '''
+    },
+]
