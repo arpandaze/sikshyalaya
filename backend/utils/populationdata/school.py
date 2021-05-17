b491faf7d8667147e73d3c23a233e2e6cd8780f6
javaclll <yugu.upadhyaya@gmail.com>
2021-05-18 04:28:11 +0545

    school population data added

---
+++ backend/utils/populationdata/school.py
@@ -0,0 +1,33 @@
+from typing import List
+from pydantic import Json
+
+schools: List[Json] = [
+    {
+        "name": "School Of Science",
+        "address": "Dhulikhel, Kavre"
+    },
+    {
+        "name": "School Of Engineering",
+        "address": "Dhulikhel, Kavre"
+    },
+    {
+        "name": "School Of Medical Sciences",
+        "address": "Dhulikhel, Kavre"
+    },
+    {
+        "name": "School Of Law",
+        "address": "Dhulikhel, Kavre"
+    },
+    {
+        "name": "School Of Management",
+        "address": "Balkumari, Lalitpur"
+    },
+    {
+        "name": "School Of Arts", "address":
+        "Hattiban, Lalitpur"
+    },
+    {
+        "name": "School Of Education", "address":
+        "Hattiban, Lalitpur"
+    },
+]
