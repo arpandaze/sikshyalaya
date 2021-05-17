ec5645a804e1cd6d0e52aa02e28f9be543c7aef4
javaclll <yugu.upadhyaya@gmail.com>
2021-05-18 04:30:25 +0545

    created real-life like population data for all models and created data imported to init

---
+++ backend/utils/populationdata/__init__.py
@@ -0,0 +1,10 @@
+from .user import users
+from .school import schools
+from .program import programs
+from .department import departments
+from .course import courses
+from .group import groups
+from .personal_note import personalNotes
+from .teacher_note import teacherNotes
+from .quiz import quizzes, quizQuestions
+from .class_session import classSessions
