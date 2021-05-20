6150edbf2dff3dcebfbbc09aa5013b225c3d789c
Daze <dazehere@yandex.com>
2021-05-20 21:11:13 +0545

    Dropdown custom field updated

--- frontend/src/components/CustomTextField.jsx
+++ frontend/src/components/CustomTextField.jsx
@@ -3,56 +3,76 @@ import { Field } from "formik";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+import { TextField } from "@material-ui/core";
 import "./statics/css/customTextField.css";
 
 const CustomTextField = ({
-  name,
-  dropdown = false,
-  type,
-  placeHolder,
-  menuItems,
-  addStyles,
-  ...rest
+    name,
+    dropdown = false,
+    type,
+    placeHolder,
+    menuItems,
+    addStyles,
+    ...rest
 }) => {
-  const isDropdown = dropdown;
-  const [option, setOption] = useState("");
+    const isDropdown = dropdown;
+    const [option, setOption] = useState("");
 
-  const handleChange = (e) => {
-    setOption(e.target.value);
-  };
+    const handleChange = (e) => {
+        setOption(e.target.value);
+    };
 
-  return !isDropdown ? (
-    <>
-      <Field name={name} {...rest}>
-        {({ field, form: { touched, errors }, meta }) => (
-          <div>
-            <input
-              type={type}
-              placeholder={placeHolder}
-              className={addStyles}
-              {...rest}
-              {...field}
-            />
-            {meta.touched && meta.error && (
-              <div className="error">{meta.error}</div>
-            )}
-          </div>
-        )}
-      </Field>
-    </>
-  ) : (
-    <>
-      <FormControl className={addStyles}>
-        <InputLabel id={name}>{placeHolder}</InputLabel>
-        <Select labelId={name} id={name} value={option} onChange={handleChange}>
-          {menuItems.map((item, index) => (
-            <MenuItem value={item.value}>{item.name}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </>
-  );
+    return !isDropdown ? (
+        <>
+            <Field name={name} {...rest}>
+                {({ field, form: { touched, errors }, meta }) => (
+                    <div>
+                        <input
+                            type={type}
+                            placeholder={placeHolder}
+                            className={addStyles}
+                            {...rest}
+                            {...field}
+                        />
+                        {meta.touched && meta.error && (
+                            <div className="error">{meta.error}</div>
+                        )}
+                    </div>
+                )}
+            </Field>
+        </>
+    ) : (
+        <>
+            <Field name={name} {...rest}>
+                {({ field, form, meta }) => (
+                    <>
+                        <TextField
+                            label={placeHolder}
+                            name={name}
+                            select
+                            placeholder={placeHolder}
+                            margin="normal"
+                            fullWidth
+                            {...rest}
+                            {...field}
+                        >
+                            <MenuItem key={""} value={""}>
+                                None
+                            </MenuItem>
+                            {menuItems.map((item, index) => (
+                                <MenuItem key={index} value={item.value}>
+                                    {item.name}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        {meta.touched && meta.error && (
+                            <div className="error">{meta.error}</div>
+                        )}
+                    </>
+                )}
+            </Field>
+        </>
+    );
 };
 
 export default CustomTextField;
