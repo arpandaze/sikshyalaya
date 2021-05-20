4b994acf7367586879e016d060bf1a62efb7f0f2
Daze <dazehere@yandex.com>
2021-05-20 21:12:27 +0545

    Delayed redirect created

---
+++ frontend/src/components/DelayedRedirect.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+
+const DelayedRedirect = ({ timeout, ...rest }) => {
+    const [redirect, setRedirect] = useState(false);
+    useEffect(() => {
+        setTimeout(() => setRedirect(true), timeout*1000);
+    });
+    return (
+        <>
+        {redirect?<Redirect {...rest}/>:null}
+        </>
+    ) 
+};
+
+export default DelayedRedirect;
