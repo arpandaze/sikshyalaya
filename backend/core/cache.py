249db5f37bdc309663a65a4119cc2164e8a15a0c
Daze <dazehere@yandex.com>
2021-05-21 22:05:53 +0545

    cache decorator update

--- backend/core/cache.py
+++ backend/core/cache.py
@@ -1,7 +1,11 @@
 from functools import wraps
+import json
+
+from starlette.responses import Response
 
 from core.db import redis_cache_client
 from fastapi import HTTPException
+from fastapi.encoders import jsonable_encoder
 from typing import Callable
 
 
@@ -9,18 +13,15 @@ def cache(timeout: int = 600) -> Callable:
     def outer_wrapper(func):
         @wraps(func)
         async def inner_wrapper(*args, **kwargs):
-            resp = kwargs.get("response")
-            print(resp.da)
             identifier = f"endpoint_cache_{func.__name__}"
             value = await redis_cache_client.client.get(identifier)
-            print(f"redis: {value}")
 
             if value:
-                return value
+                return json.loads(value)
             else:
                 ret_val = await func(*args, **kwargs)
-                print(f"ret: {ret_val}")
-                await redis_cache_client.client.set(identifier, ret_val)
+                ret_val = jsonable_encoder(ret_val)
+                await redis_cache_client.client.set(identifier, json.dumps(ret_val))
                 await redis_cache_client.client.expire(identifier, timeout)
                 return ret_val
 
