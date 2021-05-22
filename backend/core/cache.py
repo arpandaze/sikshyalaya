d159a151084e9cf157d40ce8041f13fc0cad4beb
Daze <dazehere@yandex.com>
2021-05-22 12:07:02 +0545

    function module added to identifier

--- backend/core/cache.py
+++ backend/core/cache.py
@@ -13,7 +13,7 @@ def cache(timeout: int = 600) -> Callable:
     def outer_wrapper(func):
         @wraps(func)
         async def inner_wrapper(*args, **kwargs):
-            identifier = f"endpoint_cache_{func.__name__}"
+            identifier = f"ec_{func.__name__}_{func.__module__}"
             value = await redis_cache_client.client.get(identifier)
 
             if value:
@@ -28,3 +28,27 @@ def cache(timeout: int = 600) -> Callable:
         return inner_wrapper
 
     return outer_wrapper
+
+
+# TODO: Response Cache Headers
+
+# def cache_headers(timeout: int = 600, no_cache=False, ) -> Callable:
+#     def outer_wrapper(func):
+#         @wraps(func)
+#         async def inner_wrapper(*args, **kwargs):
+#             print(func.__module__)
+#             identifier = f"ec_{func.__name__}_{func.__module__}"
+#             value = await redis_cache_client.client.get(identifier)
+
+#             if value:
+#                 return json.loads(value)
+#             else:
+#                 ret_val = await func(*args, **kwargs)
+#                 ret_val = jsonable_encoder(ret_val)
+#                 await redis_cache_client.client.set(identifier, json.dumps(ret_val))
+#                 await redis_cache_client.client.expire(identifier, timeout)
+#                 return ret_val
+
+#         return inner_wrapper
+
+#     return outer_wrapper
