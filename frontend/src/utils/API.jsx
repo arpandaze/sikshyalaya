d4938dd058d121a3c8ef12675200fb84cb08ba46
javaclll <yugu.upadhyaya@gmail.com>
2021-05-23 21:11:08 +0545

    putReq api created

--- frontend/src/utils/API.jsx
+++ frontend/src/utils/API.jsx
@@ -52,4 +52,29 @@ const postReq = async (endpoint, data, params = null) => {
     return response;
 };
 
+const putReq = async (endpoint, data, params = null) => {
+    let response = null;
+    await axios
+        .put(`${configs.API_HOST}${endpoint}`, data, {
+            withCredentials: true,
+            params: params,
+        })
+        .then((res) => {
+            response = res;
+        })
+        .catch((error) => {
+            return (response = error.response);
+        });
+
+    if (response.status === 401) {
+        del("user")
+            .then(() => {
+                window.location = "/login";
+            })
+            .catch();
+    }
+
+    return response;
+};
+
 export { getReq, postReq };
