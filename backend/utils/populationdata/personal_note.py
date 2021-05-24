06db9e15762abdddbea1f9b982862aa27a1932e5
javaclll <yugu.upadhyaya@gmail.com>
2021-05-25 00:14:46 +0545

    personal note population data changed to fix JSON Parse error during notes implementation

--- backend/utils/populationdata/personal_note.py
+++ backend/utils/populationdata/personal_note.py
@@ -1,125 +1,106 @@
 from typing import List
 from pydantic import Json
+import json
+
+
+contentAll = [
+    {"attributes": {"font": "Roboto"}, "insert": "Rushab is a very good boy"},
+    {"insert": "\n"},
+    {"attributes": {"font": "Roboto"}, "insert": "but"},
+    {"insert": "\n"},
+    {"attributes": {"font": "Roboto"}, "insert": "Yugesh isn't"},
+    {"insert": "\n"},
+]
 
 personalNotes: List[Json] = [
     {
         "user_id": 3,
-        "tags": ["Maths","Computer","Chemistry"],
-        "title" : "titleOne",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["Maths", "Computer", "Chemistry"],
+        "title": "titleOne",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 3,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "titleTwo",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "titleTwo",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 4,
-        "tags": ["Chemistry","Science","Pharma"],
-        "title" : "titleThree",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["Chemistry", "Science", "Pharma"],
+        "title": "titleThree",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 5,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title Four",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title Four",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 6,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title Five",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title Five",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 7,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title Six",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title Six",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 8,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title seven",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title seven",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 9,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title eight",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title eight",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 10,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title Nine",
-       "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title Nine",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 4,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title ten",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title ten",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 5,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title eleven",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title eleven",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 7,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title twelve",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title twelve",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 8,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title thirteen",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title thirteen",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 3,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title fourteen",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title fourteen",
+        "content": json.dumps(contentAll),
     },
     {
         "user_id": 9,
-        "tags": ["mathematics","computer","physics"],
-        "title" : "title Fifteen",
-        "content": """
-            [{"attributes":{"font":"Roboto"},"insert":"Rushab is a very good boi"},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"but "},{"insert":"\n"},{"attributes":{"font":"Roboto"},"insert":"yogesh isn't"},{"insert":"\n"}]
-        """,
+        "tags": ["mathematics", "computer", "physics"],
+        "title": "title Fifteen",
+        "content": json.dumps(contentAll),
     },
 ]
