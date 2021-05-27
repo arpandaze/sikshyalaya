c7ebd5ea2bd4de076537162018a217abe6786f74
Rushab11 <humagainrushab@gmail.com>
2021-05-27 16:18:57 +0545

    Fixed a minor issue in CustomTextField Component

--- frontend/src/components/CustomTextField.jsx
+++ frontend/src/components/CustomTextField.jsx
@@ -5,72 +5,74 @@ import { TextField } from "@material-ui/core";
 import "./statics/css/customTextField.css";
 
 const CustomTextField = ({
-  name,
-  dropdown = false,
-  type,
-  placeHolder,
-  menuItems,
-  addStyles,
-  ...rest
+	name,
+	dropdown = false,
+	type,
+	placeHolder,
+	menuItems,
+	addStyles,
+	...rest
 }) => {
-  const isDropdown = dropdown;
-  const [option, setOption] = useState("");
+	const isDropdown = dropdown;
+	const [option, setOption] = useState("");
 
-  const handleChange = (e) => {
-    setOption(e.target.value);
-  };
+	const handleChange = (e) => {
+		setOption(e.target.value);
+	};
 
-  return !isDropdown ? (
-    <>
-      <Field name={name} {...rest}>
-        {({ field, form: { touched, errors }, meta }) => (
-          <div>
-            <input
-              type={type}
-              placeholder={placeHolder}
-              className={addStyles}
-              {...rest}
-              {...field}
-            />
-            {meta.touched && meta.error && (
-              <div className="error">{meta.error}</div>
-            )}
-          </div>
-        )}
-      </Field>
-    </>
-  ) : (
-    <>
-      <Field name={name} {...rest}>
-        {({ field, form, meta }) => (
-          <>
-            <TextField
-              label={placeHolder}
-              name={name}
-              select
-              placeholder={placeHolder}
-              margin="normal"
-              fullWidth
-              {...rest}
-              {...field}
-            >
-              <MenuItem key={""} value={""}>
-                None
-              </MenuItem>
-              {menuItems.map((item, index) => (
-                <MenuItem key={index} value={item.value}>
-                  {item.name}
-                </MenuItem>
-              ))}
-            </TextField>
-            {meta.touched && meta.error && (
-              <div className="error">{meta.error}</div>
-            )}
-          </>
-        )}
-      </Field>
-    </>
-  );
+	return !isDropdown ? (
+		<>
+			<Field name={name} {...rest}>
+				{({ field, form: { touched, errors }, meta }) => (
+					<div>
+						<input
+							type={type}
+							placeholder={placeHolder}
+							className={addStyles}
+							{...rest}
+							{...field}
+						/>
+						{meta.touched && meta.error && (
+							<div className="error">{meta.error}</div>
+						)}
+					</div>
+				)}
+			</Field>
+		</>
+	) : (
+		<>
+			<Field name={name} {...rest}>
+				{({ field, form, meta }) => (
+					<>
+						<TextField
+							label={placeHolder}
+							name={name}
+							select
+							placeholder={placeHolder}
+							margin="normal"
+							fullWidth
+							{...rest}
+							{...field}
+						>
+							<MenuItem key={""} value={""}>
+								None
+							</MenuItem>
+							{menuItems &&
+								menuItems.length != 0 &&
+								menuItems.map((item, index) => (
+									<MenuItem key={index} value={item.value}>
+										{item.name}
+									</MenuItem>
+								))}
+						</TextField>
+						{meta.touched && meta.error && (
+							<div className="error">{meta.error}</div>
+						)}
+					</>
+				)}
+			</Field>
+		</>
+	);
 };
 
 export default CustomTextField;
