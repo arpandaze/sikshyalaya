efd366ad85a8bd86ed1a5298df02a2598f0c98cd
waterlulu12 <aatishshrestha123@gmail.com>
2021-05-31 19:26:46 +0545

    refactor

--- frontend/src/views/404/404.jsx
+++ frontend/src/views/404/404.jsx
@@ -8,74 +8,69 @@ import "./statics/css/404.css";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
-	return (
-		<>
-			<Grid
-				container
-				direction="column"
-				alignItems="center"
-				justifyContent="center"
-				className="notFound_root"
-				wrap="nowrap"
-			>
-				<Grid item className="notFound_logo">
-					<Link to="/login">
-						<Image
-							src={logoLarge}
-							alt="Sikshyalaya"
-							addStyles="notFound_image"
-						/>
-					</Link>
-				</Grid>
-				<Grid item className="notfound_panelContainer">
-					<Grid
-						container
-						className="notfound_404Container"
-						direction="column"
-						alignItems="center"
-						justifyContent="center"
-						spacing={3}
-					>
-						<Grid item className="notFound_404Message">
-							<a className="notFound_404giant404">404</a>
-							<a className="notFound_pageNotFound">
-								Page Not found
-							</a>
+  return (
+    <>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        className="notFound_root"
+        wrap="nowrap"
+      >
+        <Grid item className="notFound_logo">
+          <Link to="/login">
+            <Image
+              src={logoLarge}
+              alt="Sikshyalaya"
+              addStyles="notFound_image"
+            />
+          </Link>
+        </Grid>
+        <Grid item className="notfound_panelContainer">
+          <Grid
+            container
+            className="notfound_404Container"
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            spacing={3}
+          >
+            <Grid item className="notFound_404Message">
+              <a className="notFound_404giant404">404</a>
+              <a className="notFound_pageNotFound">Page Not found</a>
 
-							<p className="notFound_hmm">
-								Hmm....Seems like you're lost in a perpetual
-								black hole. Let us help guide you out and get
-								you back home.
-							</p>
-						</Grid>
-						<Grid item classname="notfound_goHome">
-							<Grid
-								container
-								direction="row"
-								alignItems="flex-start"
-								justifyContent="center"
-								spacing={2}
-							>
-								<Grid item classname="notfound_textContainer">
-									<a classname="notfound_returnText>">
-										Return to Dashboard
-									</a>
-								</Grid>
-								<Grid item classname="notFound_returnHome">
-									<Link to="/landing">
-										<FancyButton
-											color={colorscheme.purple3}
-											className="notfound_fancyButton"
-										/>
-									</Link>
-								</Grid>
-							</Grid>
-						</Grid>
-					</Grid>
-				</Grid>
-			</Grid>
-		</>
-	);
+              <p className="notFound_hmm">
+                Hmm... Seems like you're lost in a perpetual black hole. Please
+                find your way to the nearest library.
+              </p>
+            </Grid>
+            <Grid item classname="notfound_goHome">
+              <Grid
+                container
+                direction="row"
+                alignItems="flex-start"
+                justifyContent="center"
+                spacing={2}
+              >
+                <Grid item classname="notfound_textContainer">
+                  <a classname="notfound_returnText>">Return to Dashboard</a>
+                </Grid>
+                <Grid item classname="notFound_returnHome">
+                  <Link to="/landing">
+                    <FancyButton
+                      color={colorscheme.purple3}
+                      className="notfound_fancyButton"
+                    />
+                  </Link>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default NotFound;
