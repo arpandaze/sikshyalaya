ee19e5e8198bc766ec13f47d1d6e02448485c35e
waterlulu12 <aatishshrestha123@gmail.com>
2021-06-06 12:54:29 +0545

    tabs to spaces

--- frontend/src/views/Dashboard/components/DiscussionBox.jsx
+++ frontend/src/views/Dashboard/components/DiscussionBox.jsx
@@ -12,122 +12,122 @@ import { UserContext } from "../../../utils/Contexts/UserContext";
 import defaultProfile from "../../../assets/default-profile.svg";
 
 const DiscussionBox = () => {
-	const { user } = useContext(UserContext);
-	const [checked, setChecked] = useState(false);
-	const [chat, setChat] = useState([
-		{
-			id: 1,
-			name: "Rushab",
-			photo: defaultProfile,
-			text: "Hello",
-			sentTime: "2021",
-		},
-		{
-			id: 2,
-			name: "Arpan",
-			photo: defaultProfile,
-			text: "Hi",
-			sentTime: "2020",
-		},
-	]);
-	const [message, setMessage] = useState("");
-	const discussionFormatter = () => {
-		let data = {
-			id: user.id,
-			name: !checked ? user.full_name : "Anonymous",
-			photo: checked
-				? defaultProfile
-				: user.profile_image === null
-				? defaultProfile
-				: user.profile_image,
-			text: message,
-			sentTime: "2022",
-		};
-		return data;
-	};
-	const handleChange = (e) => {
-		setMessage(e.target.value);
-	};
-	const handleSubmit = () => {
-		if (message !== "") {
-			const data = discussionFormatter();
-			setChat([...chat, data]);
-			setMessage("");
-		}
-	};
+  const { user } = useContext(UserContext);
+  const [checked, setChecked] = useState(false);
+  const [chat, setChat] = useState([
+    {
+      id: 1,
+      name: "Rushab",
+      photo: defaultProfile,
+      text: "Hello",
+      sentTime: "2021",
+    },
+    {
+      id: 2,
+      name: "Arpan",
+      photo: defaultProfile,
+      text: "Hi",
+      sentTime: "2020",
+    },
+  ]);
+  const [message, setMessage] = useState("");
+  const discussionFormatter = () => {
+    let data = {
+      id: user.id,
+      name: !checked ? user.full_name : "Anonymous",
+      photo: checked
+        ? defaultProfile
+        : user.profile_image === null
+        ? defaultProfile
+        : user.profile_image,
+      text: message,
+      sentTime: "2022",
+    };
+    return data;
+  };
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+  const handleSubmit = () => {
+    if (message !== "") {
+      const data = discussionFormatter();
+      setChat([...chat, data]);
+      setMessage("");
+    }
+  };
 
-	return (
-		<>
-			<Grid
-				container
-				direction="row"
-				alignItems="center"
-				justify="center"
-				className="discussionBox_root"
-			>
-				<Grid item className="discussionBox_chatBox">
-					<Grid
-						container
-						direction="row"
-						alignItems="flex-start"
-						justify="flex-start"
-					>
-						<Grid item className="discussionBox_messageRoot">
-							<Message messages={chat} />
-						</Grid>
-					</Grid>
-				</Grid>
-				<Grid item xs={11} className="discussionBox_inputField">
-					<Grid
-						container
-						direction="row"
-						alignItems="center"
-						justify="flex-start"
-					>
-						<Grid item xs={9} className="discussionBox_textFieldRoot">
-							<input
-								name="chat_input"
-								type="text"
-								value={message}
-								onChange={handleChange}
-								placeholder="Enter your message..."
-								className="discussionBox_textField"
-							/>
-						</Grid>
-						<Grid item xs={1} className="discussionBox_sendButtonContainer">
-							<button
-								type="submit"
-								name="submit"
-								style={{
-									border: "none",
-									backgroundColor: colorscheme.white,
-								}}
-								onClick={handleSubmit}
-							>
-								<BiSend
-									size={30}
-									color={colorscheme.green3}
-									className="discussionBox_sendButton"
-								/>
-							</button>
-						</Grid>
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justify="center"
+        className="discussionBox_root"
+      >
+        <Grid item className="discussionBox_chatBox">
+          <Grid
+            container
+            direction="row"
+            alignItems="flex-start"
+            justify="flex-start"
+          >
+            <Grid item className="discussionBox_messageRoot">
+              <Message messages={chat} />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={11} className="discussionBox_inputField">
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            justify="flex-start"
+          >
+            <Grid item xs={9} className="discussionBox_textFieldRoot">
+              <input
+                name="chat_input"
+                type="text"
+                value={message}
+                onChange={handleChange}
+                placeholder="Enter your message..."
+                className="discussionBox_textField"
+              />
+            </Grid>
+            <Grid item xs={1} className="discussionBox_sendButtonContainer">
+              <button
+                type="submit"
+                name="submit"
+                style={{
+                  border: "none",
+                  backgroundColor: colorscheme.white,
+                }}
+                onClick={handleSubmit}
+              >
+                <BiSend
+                  size={30}
+                  color={colorscheme.green3}
+                  className="discussionBox_sendButton"
+                />
+              </button>
+            </Grid>
 
-						<Grid item xs={2} className="discussionBox_switchContainer">
-							<p className="discussionBox_label">Send Anonymously</p>
-							<Switch
-								name="isAnonymus"
-								checked={checked}
-								onChange={() => {
-									setChecked(!checked);
-								}}
-								className="discussionBox_switch"
-							/>
-						</Grid>
-					</Grid>
-				</Grid>
-			</Grid>
-		</>
-	);
+            <Grid item xs={2} className="discussionBox_switchContainer">
+              <p className="discussionBox_label">Send Anonymously</p>
+              <Switch
+                name="isAnonymus"
+                checked={checked}
+                onChange={() => {
+                  setChecked(!checked);
+                }}
+                className="discussionBox_switch"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default DiscussionBox;
--- frontend/src/views/Dashboard/components/Message.jsx
+++ frontend/src/views/Dashboard/components/Message.jsx
@@ -7,65 +7,65 @@ import { UserContext } from "../../../utils/Contexts/UserContext";
 import "./statics/css/message.css";
 
 const Message = ({ messages }) => {
-	const { user } = useContext(UserContext);
-	const messagesEndRef = useRef(null);
-	const scrollToBottom = () => {
-		messagesEndRef.current.scrollIntoView({ behaviour: "smooth" });
-	};
-	useEffect(() => {
-		scrollToBottom();
-	}, [messages]);
-	return (
-		<Grid
-			container
-			direction="column"
-			justify="center"
-			alignItems="flex-start"
-			className="message_root"
-		>
-			<Grid item>
-				{messages &&
-					messages.length !== 0 &&
-					messages.map((msg, index) => (
-						<>
-							<Grid item>
-								<p className="message_name">{msg.name}</p>
-							</Grid>
-							<Grid
-								container
-								direction="row"
-								alignItems="center"
-								justify="flex-start"
-							>
-								<Grid item className="message_ImageRoot">
-									<Image src={msg.photo} addStyles="message_Image" />
-								</Grid>
-								<Grid item>
-									<Grid container direction="column">
-										<Grid
-											item
-											className="message_Content"
-											style={{
-												backgroundColor:
-													user.id == msg.id
-														? colorscheme.red41
-														: colorscheme.purple2,
-											}}
-										>
-											<span style={{ color: colorscheme.white }}>
-												{/* FIX: text align not working on long single word */}
-												{msg.text}
-											</span>
-											<div ref={messagesEndRef} />
-										</Grid>
-									</Grid>
-								</Grid>
-							</Grid>
-						</>
-					))}
-			</Grid>
-		</Grid>
-	);
+  const { user } = useContext(UserContext);
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behaviour: "smooth" });
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="flex-start"
+      className="message_root"
+    >
+      <Grid item>
+        {messages &&
+          messages.length !== 0 &&
+          messages.map((msg, index) => (
+            <>
+              <Grid item>
+                <p className="message_name">{msg.name}</p>
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                alignItems="center"
+                justify="flex-start"
+              >
+                <Grid item className="message_ImageRoot">
+                  <Image src={msg.photo} addStyles="message_Image" />
+                </Grid>
+                <Grid item>
+                  <Grid container direction="column">
+                    <Grid
+                      item
+                      className="message_Content"
+                      style={{
+                        backgroundColor:
+                          user.id == msg.id
+                            ? colorscheme.red41
+                            : colorscheme.purple2,
+                      }}
+                    >
+                      <span style={{ color: colorscheme.white }}>
+                        {/* FIX: text align not working on long single word */}
+                        {msg.text}
+                      </span>
+                      <div ref={messagesEndRef} />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </>
+          ))}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Message;
