4135ac752a9ebf06abb338916151674a83c88be4
Rushab11 <humagainrushab@gmail.com>
2021-06-08 18:23:48 +0545

    TimePicker and DateTimePicker added

--- frontend/src/components/CustomDateTime.jsx
+++ frontend/src/components/CustomDateTime.jsx
@@ -4,8 +4,11 @@ import DateFnsUtils from "@date-io/date-fns";
 import {
 	MuiPickersUtilsProvider,
 	KeyboardDateTimePicker,
+	KeyboardTimePicker,
 	KeyboardDatePicker,
 } from "@material-ui/pickers";
+import { BiTimeFive } from "react-icons/bi";
+import { formatISO } from "date-fns";
 
 const DatePicker = ({ id, label, ...rest }) => {
 	return (
@@ -33,4 +36,60 @@ const DatePicker = ({ id, label, ...rest }) => {
 		</Field>
 	);
 };
-export default DatePicker;
+
+const DateTimePicker = ({ id, label, ...rest }) => {
+	return (
+		<Field name={id} {...rest}>
+			{({ field, form: { setFieldValue } }) => (
+				<>
+					<MuiPickersUtilsProvider utils={DateFnsUtils}>
+						<KeyboardDateTimePicker
+							{...field}
+							margin="normal"
+							id={id}
+							label={label}
+							minutesStep={5}
+							inputVariant="outlined"
+							value={field.value}
+							onChange={(value) => setFieldValue(id, value)}
+							KeyboardButtonProps={{
+								"aria-label": "change time",
+							}}
+							keyboardIcon={<BiTimeFive />}
+							{...rest}
+						/>
+					</MuiPickersUtilsProvider>
+				</>
+			)}
+		</Field>
+	);
+};
+
+const TimePicker = ({ id, label, ...rest }) => {
+	return (
+		<Field name={id} {...rest}>
+			{({ field, form: { setFieldValue } }) => (
+				<>
+					<MuiPickersUtilsProvider utils={DateFnsUtils}>
+						<KeyboardTimePicker
+							{...field}
+							margin="normal"
+							id={id}
+							label={label}
+							minutesStep={5}
+							inputVariant="outlined"
+							value={field.value}
+							onChange={(value) => setFieldValue(id, value)}
+							KeyboardButtonProps={{
+								"aria-label": "change time",
+							}}
+							keyboardIcon={<BiTimeFive />}
+							{...rest}
+						/>
+					</MuiPickersUtilsProvider>
+				</>
+			)}
+		</Field>
+	);
+};
+export { DatePicker, DateTimePicker, TimePicker };
