706aee490d0a2d9d651383281c75b66f89dcf4b1
Daze <dazehere@yandex.com>
2021-06-08 19:10:31 +0545

    FancyButton click fix + CustomTextField Overlap fix

--- frontend/src/components/FancyButton.jsx
+++ frontend/src/components/FancyButton.jsx
@@ -6,11 +6,7 @@ import { IconContext } from "react-icons";
 
 const FancyButton = ({ name, color, children, onSubmit, ...rest }) => {
   return (
-    <div
-      onClick={() => {
-        onSubmit();
-      }}
-    >
+    <div onClick={onSubmit}>
       <IconContext.Provider value={{ color: color, className: "buttonIcon" }}>
         <BsArrowRightShort />
       </IconContext.Provider>
