8ae95e572af213720600c510359277853b90a25b
Rushab11 <humagainrushab@gmail.com>
2021-06-08 15:22:57 +0545

    minor

--- frontend/src/views/Dashboard/components/Message.jsx
+++ frontend/src/views/Dashboard/components/Message.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useRef } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import Grid from "@material-ui/core/Grid";
 import "./statics/css/discussionBox.css";
 import colorscheme from "../../../utils/colors";
@@ -7,65 +7,67 @@ import { UserContext } from "../../../utils/Contexts/UserContext";
 import "./statics/css/message.css";
 
 const Message = ({ messages }) => {
-  const { user } = useContext(UserContext);
-  const messagesEndRef = useRef(null);
-  const scrollToBottom = () => {
-    messagesEndRef.current.scrollIntoView({ behaviour: "smooth" });
-  };
-  useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
-  return (
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignItems="flex-start"
-      className="message_root"
-    >
-      <Grid item>
-        {messages &&
-          messages.length !== 0 &&
-          messages.map((msg, index) => (
-            <>
-              <Grid item>
-                <p className="message_name">{msg.name}</p>
-              </Grid>
-              <Grid
-                container
-                direction="row"
-                alignItems="center"
-                justify="flex-start"
-              >
-                <Grid item className="message_ImageRoot">
-                  <Image src={msg.photo} addStyles="message_Image" />
-                </Grid>
-                <Grid item>
-                  <Grid container direction="column">
-                    <Grid
-                      item
-                      className="message_Content"
-                      style={{
-                        backgroundColor:
-                          user.id == msg.id
-                            ? colorscheme.red41
-                            : colorscheme.purple2,
-                      }}
-                    >
-                      <span style={{ color: colorscheme.white }}>
-                        {/* FIX: text align not working on long single word */}
-                        {msg.text}
-                      </span>
-                      <div ref={messagesEndRef} />
-                    </Grid>
-                  </Grid>
-                </Grid>
-              </Grid>
-            </>
-          ))}
-      </Grid>
-    </Grid>
-  );
+	const { user } = useContext(UserContext);
+	const messagesEndRef = useRef(null);
+	const scrollToBottom = () => {
+		messagesEndRef.current.scrollIntoView({ behaviour: "smooth" });
+	};
+	useEffect(() => {
+		scrollToBottom();
+	}, [messages]);
+	return (
+		<Grid
+			container
+			direction="column"
+			justify="center"
+			alignItems="flex-start"
+			className="message_root"
+		>
+			<Grid item>
+				{messages &&
+					messages.length !== 0 &&
+					messages.map((msg, index) => (
+						<>
+							<Grid item>
+								<p className="message_name">{msg.name}</p>
+							</Grid>
+							<Grid
+								container
+								direction="row"
+								alignItems="center"
+								justify="flex-start"
+							>
+								<Grid item className="message_ImageRoot">
+									<Image src={msg.photo} addStyles="message_Image" />
+								</Grid>
+								<Grid item>
+									<Grid container direction="column">
+										<Grid
+											item
+											className="message_Content"
+											style={{
+												backgroundColor:
+													user.id == msg.id
+														? colorscheme.red41
+														: colorscheme.purple2,
+											}}
+										>
+											<span style={{ color: colorscheme.white }}>
+												{msg.text}
+											</span>
+											<div ref={messagesEndRef} />
+										</Grid>
+									</Grid>
+								</Grid>
+								<Grid item>
+									<p className="message_time">{msg.sentTime}</p>
+								</Grid>
+							</Grid>
+						</>
+					))}
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Message;
