c7f63dce8acfe2fa59e29ba96023a05795e31dc0
Rushab11 <humagainrushab@gmail.com>
2021-06-10 13:36:26 +0545

    questions component [quiz creator]

--- frontend/src/views/QuizCreator/components/Question.jsx
+++ frontend/src/views/QuizCreator/components/Question.jsx
@@ -1,107 +1,66 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
-import { Formik, Form, Field, FieldArray } from "formik";
-import "./statics/css/questions.css";
-import { GoPlus } from "react-icons/go";
-import { BsCloudUpload } from "react-icons/bs";
-import { ImCross } from "react-icons/im";
-import colorscheme from "../../../utils/colors";
-import CustomTextField from "./../../../components/CustomTextField";
+import { Field, FieldArray } from "formik";
+import "../statics/css/quizCreator.css";
+import QuestionForm from "./QuestionForm";
 
-let answerList = [];
+const Questions = ({ name, val, answerList, reference, ...rest }) => {
+  const handleScroll = () => {
+    if (reference.current) {
+      reference.current.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  };
+  useEffect(() => {
+    handleScroll();
+  }, [val && val.length]);
 
-const Questions = ({ question, index, ...rest }) => {
   const questionInitialValue = {
     question_text: "",
-    question_type: "",
+    question_image: "",
     options: [],
   };
   return (
-    <Grid
-      container
-      direction="column"
-      className="questions_root"
-      alignItems="flex-start"
-      spacing={2}
-    >
-      <Grid item className="questions_innerContainer">
-        <Grid container direction="column" alignItems="flex-start">
-          <Grid item className="questions_titleTextContainer">
-            <p className="questions_titleText">Question #{index + 1}</p>
-          </Grid>
-
-          <Grid item className="questions_questionTitleConatiner">
-            <CustomTextField
-              name={`questions[${index}].question_text`}
-              type="text"
-              placeHolder="Question Title"
-            />
-          </Grid>
-          <Grid item className="questions_optionsContainer">
-            <p className="questions_optionsText">
-              <FieldArray name={`questions[${index}].options`}>
-                {(newHelper) => (
+    <Field name={name}>
+      {({ field }) => (
+        <FieldArray name={name}>
+          {(questionHelper) => (
+            <>
+              {val &&
+                val.length !== 0 &&
+                val.map((question, index) => (
                   <>
-                    <button
-                      type="button"
-                      onClick={() => {
-                        newHelper.push();
-                        answerList[index].push({
-                          name: `Option ${question.options.length + 1}`,
-                          value: question.options.length + 1,
-                        });
-                      }}
-                    >
-                      Add Options
-                    </button>
-                    {question.options &&
-                      question.options.length !== 0 &&
-                      question.options.map((option, optionIndex) => (
-                        <div key={optionIndex}>
-                          <div>
-                            <CustomTextField
-                              name={`questions[${index}].options[${optionIndex}]`}
-                              placeHolder={`Option ${optionIndex + 1}`}
-                            />
-                          </div>
-                        </div>
-                      ))}
-
-                    <CustomTextField
-                      dropdown={true}
-                      name={`questions[${index}].answer`}
-                      menuItems={answerList[index] || []}
-                      placeHolder="Choose correct Option"
-                    />
+                    <Grid container direction="column" wrap="nowrap">
+                      <div key={index + 1}>
+                        <QuestionForm
+                          name={name}
+                          index={index}
+                          question={question}
+                          questionHelper={questionHelper}
+                          answerList={answerList}
+                        />
+                      </div>
+                    </Grid>
                   </>
-                )}
-              </FieldArray>
-            </p>
-          </Grid>
-        </Grid>
-      </Grid>
-
-      <Grid item className="questions_MenuContainer">
-        <Grid
-          container
-          direction="column"
-          alignItems="flex-start"
-          justify="center"
-          classname="questions_Menu"
-          spacing={3}
-        >
-          <Grid item className="questions_plus">
-            <GoPlus color={colorscheme.green2} size={30} />
-          </Grid>
-          <Grid item className="questions_fileUpload">
-            <BsCloudUpload color={colorscheme.purple2} size={30} />
-          </Grid>
-          <Grid item className="questions_removeQuestion">
-            <ImCross color={colorscheme.red2} size={25} />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
+                ))}
+              <Grid item className="quizCreator_addQuestionButtonContainer">
+                <button
+                  type="button"
+                  className="quizCreator_addQuestionButton"
+                  onClick={() => {
+                    questionHelper.push(questionInitialValue);
+                    answerList.push([]);
+                  }}
+                >
+                  Add Question
+                </button>
+              </Grid>
+            </>
+          )}
+        </FieldArray>
+      )}
+    </Field>
   );
 };
 
