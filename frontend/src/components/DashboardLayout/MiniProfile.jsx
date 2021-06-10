ff2718efada862df120519a8df8d1eb9a69dedd9
avgt93 <abhijeetpoudel@gmail.com>
2021-06-10 17:21:39 +0545

    MIniprofile aaile ko lagi temporary fix

---
+++ frontend/src/components/DashboardLayout/MiniProfile.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import Grid from "@material-ui/core/Grid";
+import colorscheme from "../../utils/colors";
+import Image from "../Image";
+import { UserContext } from "../../utils/Contexts/UserContext";
+import "./statics/css/miniProfile.css";
+import configs from "../../utils/configs";
+
+const MiniProfile = ({
+	id,
+	username,
+	src,
+	program,
+	semester,
+	year,
+	...rest
+}) => {
+	return (
+		<Grid
+			container
+			direction="column"
+			justify="center"
+			alignItems="center"
+			className="miniProfile_root"
+		>
+			<Grid item className="miniProfile_ImageRoot">
+				<div className="miniProfile_greenDot"></div>
+				<Image src={src} addStyles="miniProfile_Image" />
+			</Grid>
+			<Grid item className="miniProfile_nameContainer">
+				<p className="miniProfile_name">{username}</p>
+			</Grid>
+			<Grid item className="miniProfile_descriptionOuter">
+				<p className="miniProfile_description">
+					{program}
+					<br />
+					{year} Year /{semester} Semester
+				</p>
+			</Grid>
+		</Grid>
+	);
+};
+
+export default MiniProfile;
--- frontend/src/views/Dashboard/components/Message.jsx
+++ frontend/src/views/Dashboard/components/Message.jsx
@@ -9,94 +9,76 @@ import configs from "../../../utils/configs";
 import defaultProfile from "../../../assets/default-profile.svg";
 
 const Message = ({ messages }) => {
-  const [hovered, setHovered] = useState(false);
-  const { user } = useContext(UserContext);
-  const messagesEndRef = useRef(null);
-  const scrollToBottom = () => {
-    if (messagesEndRef.current) {
-      messagesEndRef.current.scrollIntoView({ behaviour: "smooth" });
-    }
-  };
-  useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
-  return (
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignItems="flex-start"
-      className="message_root"
-    >
-      <Grid item>
-        {messages &&
-          messages.length !== 0 &&
-          messages.map((msg, index) => (
-            <>
-              <Grid item>
-                <p className="message_name">{msg.name}</p>
-              </Grid>
-              <Grid
-                container
-                direction="row"
-                alignItems="center"
-                justify="flex-start"
-              >
-                <Grid item className="message_ImageRoot">
-                  <Image
-                    src={
-                      msg.photo
-                        ? `${configs.PUBLIC_FILES_PATH}/${msg.photo}`
-                        : defaultProfile
-                    }
-                    addStyles="message_Image"
-                  />
-                </Grid>
-                <Grid item>
-                  <Grid container direction="row" wrap="nowrap">
-                    <Grid
-                      item
-                      className="message_Content"
-                      style={{
-                        backgroundColor:
-                          user.id == msg.id
-                            ? colorscheme.red41
-                            : colorscheme.purple2,
-                      }}
-                      onMouseEnter={() => {
-                        setHovered(true);
-                      }}
-                      onMouseLeave={() => {
-                        setHovered(false);
-                      }}
-                    >
-                      <span style={{ color: colorscheme.white }}>
-                        {msg.text}
-                      </span>
-                      <div ref={messagesEndRef} />
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <Grid
-                  item
-                  style={
-                    !hovered
-                      ? {
-                          display: "none",
-                        }
-                      : {
-                          display: "block",
-                        }
-                  }
-                >
-                  <p className="message_time">{msg.sentTime}</p>
-                </Grid>
-              </Grid>
-            </>
-          ))}
-      </Grid>
-    </Grid>
-  );
+	const { user } = useContext(UserContext);
+	const messagesEndRef = useRef(null);
+	const scrollToBottom = () => {
+		if (messagesEndRef.current) {
+			messagesEndRef.current.scrollIntoView({ behaviour: "smooth" });
+		}
+	};
+	useEffect(() => {
+		scrollToBottom();
+	}, [messages]);
+	return (
+		<Grid
+			container
+			direction="column"
+			justify="center"
+			alignItems="flex-start"
+			className="message_root"
+		>
+			<Grid item>
+				{messages &&
+					messages.length !== 0 &&
+					messages.map((msg, index) => (
+						<>
+							<Grid item>
+								<p className="message_name">{msg.name}</p>
+							</Grid>
+							<Grid
+								container
+								direction="row"
+								alignItems="center"
+								justify="flex-start"
+							>
+								<Grid item className="message_ImageRoot">
+									<Image
+										src={
+											msg.photo
+												? `${configs.PUBLIC_FILES_PATH}/${msg.photo}`
+												: defaultProfile
+										}
+										addStyles="message_Image"
+									/>
+								</Grid>
+								<Grid item className="message_messageOuter">
+									<Grid container direction="row" wrap="nowrap">
+										<Grid
+											item
+											className="message_Content"
+											style={{
+												backgroundColor:
+													user.id == msg.id
+														? colorscheme.red41
+														: colorscheme.purple2,
+											}}
+										>
+											<span style={{ color: colorscheme.white }}>
+												{msg.text}
+											</span>
+											<div ref={messagesEndRef} />
+											<Grid item className="message_timeContainer">
+												<p className="message_time">{msg.sentTime}</p>
+											</Grid>
+										</Grid>
+									</Grid>
+								</Grid>
+							</Grid>
+						</>
+					))}
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Message;
