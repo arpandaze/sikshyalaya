4ebf9e15918d2fe5bb051d9684d6211c57f02fb3
avgt93 <abhijeetpoudel@gmail.com>
2021-06-13 23:18:28 +0545

    Quiz Creator completed!!!!!!!!!!!!!!!!

--- frontend/src/components/CustomTextField.jsx
+++ frontend/src/components/CustomTextField.jsx
@@ -5,78 +5,80 @@ import { TextField } from "@material-ui/core";
 import "./statics/css/customTextField.css";
 
 const CustomTextField = ({
-  name,
-  dropdown = false,
-  type,
-  placeHolder,
-  menuItems,
-  addStyles,
-  ...rest
+	name,
+	dropdown = false,
+	type,
+	placeHolder,
+	menuItems,
+	addStyles,
+	value,
+	...rest
 }) => {
-  const isDropdown = dropdown;
-  const [option, setOption] = useState("");
+	const isDropdown = dropdown;
+	const [option, setOption] = useState("");
 
-  const handleChange = (e) => {
-    setOption(e.target.value);
-  };
+	const handleChange = (e) => {
+		setOption(e.target.value);
+	};
 
-  return !isDropdown ? (
-    <>
-      <Field name={name} {...rest}>
-        {({ field, form: { touched, errors }, meta }) => (
-          <div className="customField_root">
-            <TextField
-              error={meta.touched && meta.error ? true : false}
-              id={name}
-              type={type}
-              margin="normal"
+	return !isDropdown ? (
+		<>
+			<Field name={name} {...rest}>
+				{({ field, form: { touched, errors }, meta }) => (
+					<div className="customField_root">
+						<TextField
+							error={meta.touched && meta.error ? true : false}
+							id={name}
+							type={type}
+							margin="normal"
               label={placeHolder}
-              variant="outlined"
-              className={addStyles}
-              {...rest}
-              {...field}
-              helperText={meta.touched && meta.error}
-            />
-          </div>
-        )}
-      </Field>
-    </>
-  ) : (
-    <>
-      <Field name={name} {...rest}>
-        {({ field, form, meta }) => (
-          <div className="customField_root">
-            <TextField
-              label={placeHolder}
-              name={name}
-              select
-              placeholder={placeHolder}
-              margin="normal"
-              fullWidth
-              InputLabelProps={{ shrink: true }}
-              {...rest}
-              {...field}
-              variant="outlined"
-            >
-              <MenuItem key={""} value={""}>
-                None
-              </MenuItem>
-              {menuItems &&
-                menuItems.length != 0 &&
-                menuItems.map((item, index) => (
-                  <MenuItem key={index} value={item.value}>
-                    {item.name}
-                  </MenuItem>
-                ))}
-            </TextField>
-            {meta.touched && meta.error && (
-              <div className="error">{meta.error}</div>
-            )}
-          </div>
-        )}
-      </Field>
-    </>
-  );
+              value={value}
+							variant="outlined"
+							className={addStyles}
+							{...rest}
+							{...field}
+							helperText={meta.touched && meta.error}
+						/>
+					</div>
+				)}
+			</Field>
+		</>
+	) : (
+		<>
+			<Field name={name} {...rest}>
+				{({ field, form, meta }) => (
+					<div className="customField_root">
+						<TextField
+							label={placeHolder}
+							name={name}
+							select
+							placeholder={placeHolder}
+							margin="normal"
+							fullWidth
+							InputLabelProps={{ shrink: true }}
+							{...rest}
+							{...field}
+							variant="outlined"
+						>
+							<MenuItem key={""} value={""}>
+								None
+							</MenuItem>
+							{menuItems &&
+								menuItems.length != 0 &&
+								menuItems.map((item, index) => (
+									<MenuItem key={index} value={item.value}>
+										{item.name}
+									</MenuItem>
+								))}
+						</TextField>
+						{meta.touched && meta.error && (
+							<div className="error">{meta.error}</div>
+						)}
+					</div>
+				)}
+			</Field>
+		</>
+	);
 };
 
 export default CustomTextField;
--- frontend/src/views/QuizCreator/components/QuestionForm.jsx
+++ frontend/src/views/QuizCreator/components/QuestionForm.jsx
@@ -11,6 +11,7 @@ import { DropzoneDialog } from "material-ui-dropzone";
 import Options from "./Options";
 import { FileUpload } from "../../../components/FileUpload";
 import { QuizContext } from "../QuizContext";
+import { MdExpandMore } from "react-icons/md";
 
 const QuestionForm = ({
   name,
@@ -37,78 +38,94 @@ const QuestionForm = ({
     setSelectedFile(temp);
   };
   return (
-    <>
-      <Grid item>
-        <Grid
-          container
-          directon="row"
-          justify="center"
-          alignItems="center"
-          wrap="nowrap"
-          className="quizCreator_questionTitleContainer"
-          onClick={() => {
-            setMinimized(!minimized);
-          }}
-        >
-          <Grid item xs={12}>
-            <h4 className="quizCreator_questionNumber">
-              Question #{index + 1}
-            </h4>
-          </Grid>
-          <Grid item className="quizCreator_upload" xs={3}>
-            <Grid
-              container
-              direction="row"
-              alignItems="center"
-              justify="center"
-            >
-              <FileUpload
-                label="Upload Question Image"
-                acceptedFiles={["image/jpeg", "image/png"]}
-                handleSave={(files) => {
-                  handleUploadSave(files);
-                }}
-                maxFiles={4}
-              />
-            </Grid>
-          </Grid>
+		<>
+			<Grid item>
+				<Grid
+					container
+					directon="row"
+					justify="center"
+					alignItems="center"
+					wrap="nowrap"
+					className="quizCreator_questionTitleContainer"
+					onClick={() => {
+						setMinimized(!minimized);
+					}}
+				>
+					<MdExpandMore
+						size={30}
+						color={colorscheme.grey4}
+						className="minimizeicon"
+						style={
+							minimized
+								? {
+										transform: "rotate(-90deg)",
+										transition: "350ms ease",
+								  }
+								: {
+										transform: "rotate(0deg)",
+										transition: "350ms ease",
+								  }
+						}
+					/>
+					<Grid item xs={12}>
+						<h4 className="quizCreator_questionNumber">
+							Question #{index + 1}
+						</h4>
+					</Grid>
+					<Grid item className="quizCreator_upload" xs={3}>
+						<Grid
+							container
+							direction="row"
+							alignItems="center"
+							justify="center"
+						>
+							<FileUpload
+								label="Upload File"
+								acceptedFiles={["image/jpeg", "image/png"]}
+								handleSave={(files) => {
+									handleUploadSave(files);
+								}}
+								maxFiles={4}
+							/>
+						</Grid>
+					</Grid>
 
-          <Grid item xs={1} className="quizCreator_removeContainer">
-            <button
-              type="button"
-              title="Remove Question"
-              className="quizCreator_remove"
-              onClick={() => {
-                questionHelper.remove(index);
-                answerList.splice(index, 1);
-              }}
-            >
-              <ImCross size={20} color={colorscheme.red2} />
-            </button>
-          </Grid>
-        </Grid>
-      </Grid>
-      <div style={{ display: minimized ? "none" : "" }}>
-        <Grid item xs={12} className="quizCreator_questionTitleOuter">
-          <CustomTextField
-            name={`${name}[${index}].question_text`}
-            addStyles="quizCreator_questionTitle"
-            type="text"
-            placeHolder="Question Title"
-            autoComplete="off"
-          />
-        </Grid>
-        <Grid container direction="column" alignItems="center" justify="center">
-          <Options
-            optionName={name}
-            question={question}
-            index={index}
-            answer={answerList}
-          />
-        </Grid>
-      </div>
-    </>
-  );
+					<Grid item xs={1} className="quizCreator_removeContainer">
+						<button
+							type="button"
+							title="Remove Question"
+							className="quizCreator_remove"
+							onClick={() => {
+								questionHelper.remove(index);
+								answerList.splice(index, 1);
+							}}
+						>
+							<ImCross size={20} color={colorscheme.red2} />
+						</button>
+					</Grid>
+				</Grid>
+			</Grid>
+			<div style={{ display: minimized ? "none" : "" }}>
+				<Grid item xs={12} className="quizCreator_questionTitleOuter">
+					<CustomTextField
+						name={`${name}[${index}].question_text`}
+						addStyles="quizCreator_questionTitle"
+						type="text"
+						placeHolder="Question Title"
+						autoComplete="off"
+					/>
+				</Grid>
+				<Grid container direction="column" alignItems="center" justify="center">
+					<Options
+						optionName={name}
+						question={question}
+						index={index}
+						answer={answerList}
+					/>
+				</Grid>
+			</div>
+		</>
+	);
 };
 
 export default QuestionForm;
