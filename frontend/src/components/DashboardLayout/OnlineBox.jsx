3ab64ae3962ce03e7abc0be999dbe246bcef0e5a
waterlulu12 <aatishshrestha123@gmail.com>
2021-06-15 16:43:23 +0545

    small fix online box

--- frontend/src/components/DashboardLayout/OnlineBox.jsx
+++ frontend/src/components/DashboardLayout/OnlineBox.jsx
@@ -8,89 +8,6 @@ import { UserContext } from "../../utils/Contexts/UserContext";
 import configs from "../../utils/configs";
 import defaultProfile from "../../assets/default-profile.svg";
 
-const users = [
-  {
-    id: 1,
-    name: "Yughyaya Luitel",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-  {
-    id: 2,
-    name: "Yugesh Upadhyitel",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-  {
-    id: 3,
-    name: "Yugesh Upaya Luitel",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-  {
-    id: 4,
-    name: "Yugesh Upadh",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-  {
-    id: 5,
-    name: "Yugesh Upadhyaitel",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-  {
-    id: 6,
-    name: "Yugesh Upadhytel",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-  {
-    id: 7,
-    name: "Yugesh Upadteasdlsasasadasdadadas",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-  {
-    id: 8,
-    name: "Yugeadhyaya Luitel",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-  {
-    id: 9,
-    name: "Yugesh Upa Luitel",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-  {
-    id: 10,
-    name: "Yuga Luitel",
-    image: Profile,
-    program: "Computer Science",
-    year: "1st",
-    semester: "II",
-  },
-];
-
 var end = ["st", "nd", "rd", "th", "th"];
 const OnlineBox = ({ miniProfile = true, size, ...rest }) => {
   const {
@@ -136,48 +53,38 @@ const OnlineBox = ({ miniProfile = true, size, ...rest }) => {
     }
   }, [classmatesState, JSON.stringify(onlineState)]);
   return (
-		<Grid
-			container
-			direction="row"
-			className="onlineBox_root"
-			alignItems="center"
-		>
-			<Grid item className="online_titleBar">
-				<Grid container direction="row">
-					<Grid item>
-						<div className="onlineBox_greenDot"></div>
-					</Grid>
-					<Grid item>
-						<p className="onlineBox_courseTextTitle">Online Users</p>
-					</Grid>
-				</Grid>
-			</Grid>
-
-			<Grid item xs={12} className="onlineBox_userlistContainerOuter">
-				<Grid
-					container
-					direction="column"
-					className="onlineBox_userlistContainer"
-				>
-					{classmates.map((user) => (
-						<Grid item className="onlineBox_userList">
-							<Online
-								id={user.id}
-								username={user.name}
-								src={user.image}
-								year={user.year}
-								online={user.online}
-								semester={user.semester}
-								program={user.program}
-								miniProfile={miniProfile}
-								size={size}
-							/>
-						</Grid>
-					))}
-				</Grid>
-			</Grid>
-		</Grid>
-	);
+    <Grid container direction="column" className="onlineBox_root">
+      <Grid item className="online_titleBar">
+        <Grid container direction="row" alignItems="center">
+          <div className="onlineBox_greenDot"></div>
+          <p className="onlineBox_courseTextTitle">Online Users</p>
+        </Grid>
+      </Grid>
+      <Grid item className="onlineBox_userlistContainerOuter">
+        <Grid
+          container
+          direction="column"
+          className="onlineBox_userlistContainer"
+        >
+          {classmates.map((user) => (
+            <Grid item className="onlineBox_userList">
+              <Online
+                id={user.id}
+                username={user.name}
+                src={user.image}
+                year={user.year}
+                online={user.online}
+                semester={user.semester}
+                program={user.program}
+                miniProfile={miniProfile}
+                size={size}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default OnlineBox;
