a939e98cf6f17f2c7d55081f6086f210cbc92357
waterlulu12 <aatishshrestha123@gmail.com>
2021-06-17 20:49:48 +0545

    quiz viewer in progress

--- frontend/src/views/Quiz/components/QuestionView.jsx
+++ frontend/src/views/Quiz/components/QuestionView.jsx
@@ -1,52 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import "./statics/css/questionView.css";
 import "../../../components/CustomCheckboxComponent";
-import { Formik, Form } from "formik";
-import Checkbox from "../../../components/Checkbox";
+import QuizAnswer from "./QuizAnswer";
+import { ImCross } from "react-icons/im";
+import Image from "../../../components/Image";
+import configs from "../../../utils/configs";
+import colorscheme from "../../../utils/colors";
 
-const QuestionView = ({ data, position, length, ...rest }) => {
-  const onSubmit = async (values) => {};
+const QuestionView = ({ data, position, length, multiple, ...rest }) => {
+  const [isPopUp, setIsPopUp] = useState(false);
+  const [popUpImage, setPopUpImage] = useState("");
+  const handleImagePopUp = (image) => {
+    setIsPopUp(true);
+    setPopUpImage(configs.PUBLIC_FILES_PATH + "/" + image);
+  };
   return (
-    <Grid container direction="column" className="questionView_root">
-      <Grid item className="questionView_top">
+    <>
+      <Grid container direction="column" className="questionView_root">
+        <Grid item className="questionView_top">
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            className="questionView_topInside"
+          >
+            <Grid item className="questionView_titleLabel">
+              Question {position + 1} of {length}{" "}
+            </Grid>
+            <Grid item className="questionView_questionTitle">
+              {data.question_text}
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          className={
+            data.question_image && data.question_image.length
+              ? "questionView_questionImageContainer"
+              : "questionView_questionImageContainerDisabled"
+          }
+        >
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            className="questionView_questionImageContainerRed"
+          >
+            {data.question_image && data.question_image.length ? (
+              data.question_image.map((image) => (
+                <Grid
+                  item
+                  className="questionView_questionImageContainerInside"
+                >
+                  <Image
+                    src={configs.PUBLIC_FILES_PATH + "/" + image}
+                    className="questionView_questionImage"
+                    onClick={() => {
+                      handleImagePopUp(image);
+                    }}
+                  />
+                </Grid>
+              ))
+            ) : (
+              <></>
+            )}
+          </Grid>
+        </Grid>
+        <Grid item className="questionView_bot">
+          <Grid container direction="column">
+            <Grid container direction="column" alignItems="flex-start">
+              {!multiple ? (
+                <>
+                  <p className="questionView_quizOptionTitle">
+                    Select One Option
+                  </p>
+                  <QuizAnswer
+                    options={data.options}
+                    onPopUp={handleImagePopUp}
+                    name={`questions.${data.id}`}
+                  />
+                </>
+              ) : (
+                <>
+                  <p className="questionView_quizOptionTitle">
+                    Select Multiple Option(s)
+                  </p>
+                  <QuizAnswer
+                    options={data.options}
+                    onPopUp={handleImagePopUp}
+                    name={`questions.${data.id}`}
+                    multiple={true}
+                  />
+                </>
+              )}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      {isPopUp ? (
         <Grid
           container
-          direction="column"
           justify="center"
-          className="questionView_topInside"
+          className="questionView_popUpContainer"
         >
-          <Grid item className="questionView_titleLabel">
-            Question {position + 1} of {length}{" "}
-          </Grid>
-          <Grid item className="questionView_questionTitle">
-            {data.question_text}
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item className="questionView_bot">
-        <Grid container direction="column">
-          <Formik
-            initialValues={{
-              email: "",
-              password: "",
-              remember_me: false,
-            }}
-            onSubmit={onSubmit}
-          >
-            <Form>
-              <Grid container direction="column" alignItems="flex-start">
-                {data.options.map((option, index) => (
-                  <Grid item>
-                    <Checkbox name={index} label={option.text} value={index} />
-                  </Grid>
-                ))}
+          <Grid item className="questionView_popUpBox">
+            <Grid container direction="column">
+              <Image
+                src={popUpImage != "" ? popUpImage : ""}
+                className="questionView_questionPopUpImage"
+              />
+
+              <Grid item className="questionView_closeButtonContainer">
+                <ImCross
+                  color={colorscheme.red3}
+                  className="questionView_closeButton"
+                  onClick={() => {
+                    setIsPopUp(false);
+                  }}
+                />
               </Grid>
-            </Form>
-          </Formik>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
-    </Grid>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
