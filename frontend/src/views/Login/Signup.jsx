1e0e133218387ec4b50efebcd36699d1168341da
waterlulu12 <aatishshrestha123@gmail.com>
2021-06-17 20:50:20 +0545

    removed unnecessary console.log

--- frontend/src/views/Login/Signup.jsx
+++ frontend/src/views/Login/Signup.jsx
@@ -71,6 +71,7 @@ const Signup = () => {
   const [group] = useAPI({ endpoint: "/api/v1/group/all/" }, groupFormatter);
 
   const onSubmit = async (data, { setErrors }) => {
+    console.log(data);
     let group_id_list = group.filter((item) => {
       if (item.sem === data.semester && item.program.id === data.program) {
         return item;
@@ -132,7 +133,7 @@ const Signup = () => {
                 program: "",
                 semester: "",
                 join_year: "",
-                dob: "",
+                dob: null,
                 email: "",
                 password: "",
                 confirm_password: "",
