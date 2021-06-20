3b28cf025643600e55ce339d4e8638d4a86906ee
javaclll <yugu.upadhyaya@gmail.com>
2021-06-20 20:07:10 +0545

    quiz answer population data created

---
+++ backend/utils/populationdata/quiz_answer.py
@@ -0,0 +1,38 @@
+from typing import List
+from pydantic import Json
+
+quizAnswers: List[Json] = [
+    {
+        "student_id": 3,
+        "quiz_id": 11,
+        "marks_obtained": 20,
+        "options_selected": {
+            "25": [1],
+            "26": [2],
+            "27": [0],
+            "28": [0, 2, 3],
+        },
+    },
+    {
+        "student_id": 2,
+        "quiz_id": 11,
+        "marks_obtained": 20,
+        "options_selected": {
+            "25": [1],
+            "26": [0],
+            "27": [2],
+            "28": [0, 2],
+        },
+    },
+    {
+        "student_id": 4,
+        "quiz_id": 11,
+        "marks_obtained": 12,
+        "options_selected": {
+            "25": [1],
+            "26": [0],
+            "27": [2],
+            "28": [0, 2],
+        },
+    },
+]
