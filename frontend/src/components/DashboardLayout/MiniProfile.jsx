53ce2dcebe0e81f61211d4c82b773c06854511a7
waterlulu12 <aatishshrestha123@gmail.com>
2021-06-21 17:04:17 +0545

    miniProfile in progress

---
+++ frontend/src/components/DashboardLayout/MiniProfile.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import Grid from "@material-ui/core/Grid";
+import Image from "../Image";
+import "./statics/css/miniProfile.css";
+
+const MiniProfile = ({ id, username, src, year, semester, program }) => {
+  return (
+    <Grid container direction="row" className="mini_root" alignItems="center">
+      <Grid item className="mini_imageRoot">
+        <Image src={src} addStyles="mini_image" />
+      </Grid>
+      <Grid item className="mini_nameContainer">
+        <Grid container direction="column">
+          <p className="mini_nameText">{username}</p>
+          <p className="mini_nameText">
+            {year} Year {semester} Semester
+          </p>
+          <p className="mini_nameText">{program}</p>
+        </Grid>
+      </Grid>
+      <Grid item></Grid>
+    </Grid>
+  );
+};
+
+export default MiniProfile;
