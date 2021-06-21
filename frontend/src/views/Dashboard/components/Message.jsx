f23f4489b6e885a5016e53bc7f18b408f26e34fb
Rushab11 <humagainrushab@gmail.com>
2021-06-22 00:58:16 +0545

    discussion box fix in progress

--- frontend/src/views/Dashboard/components/Message.jsx
+++ frontend/src/views/Dashboard/components/Message.jsx
@@ -7,78 +7,81 @@ import { UserContext } from "../../../utils/Contexts/UserContext";
 import "./statics/css/message.css";
 import configs from "../../../utils/configs";
 import defaultProfile from "../../../assets/default-profile.svg";
+import { format } from "date-fns";
 
 const Message = ({ messages }) => {
-	const { user } = useContext(UserContext);
-	const messagesEndRef = useRef(null);
-	const scrollToBottom = () => {
-		if (messagesEndRef.current) {
-			messagesEndRef.current.scrollIntoView({ behaviour: "smooth" });
-		}
-	};
-	useEffect(() => {
-		scrollToBottom();
-	}, [messages]);
-	return (
-		<Grid
-			container
-			direction="column"
-			justify="center"
-			alignItems="flex-start"
-			className="message_root"
-		>
-			<Grid item>
-				{messages &&
-					messages.length !== 0 &&
-					messages.map((msg, index) => (
-						<>
-							<Grid item>
-								<p className="message_name">{msg.name}</p>
-							</Grid>
-							<Grid
-								container
-								direction="row"
-								alignItems="center"
-								justify="flex-start"
-							>
-								<Grid item className="message_ImageRoot">
-									<Image
-										src={
-											msg.photo
-												? `${configs.PUBLIC_FILES_PATH}/${msg.photo}`
-												: defaultProfile
-										}
-										addStyles="message_Image"
-									/>
-								</Grid>
-								<Grid item className="message_messageOuter">
-									<Grid container direction="row" wrap="nowrap">
-										<Grid
-											item
-											className="message_Content"
-											style={{
-												backgroundColor:
-													user.id == msg.id
-														? colorscheme.red41
-														: colorscheme.purple2,
-											}}
-										>
-											<span style={{ color: colorscheme.white }}>
-												{msg.text}
-											</span>
-											<div ref={messagesEndRef} />
-											<Grid item className="message_timeContainer">
-												<p className="message_time">{msg.sentTime}</p>
-											</Grid>
-										</Grid>
-									</Grid>
-								</Grid>
-							</Grid>
-						</>
-					))}
-			</Grid>
-		</Grid>
-	);
+  const { user } = useContext(UserContext);
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behaviour: "smooth" });
+    }
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="flex-start"
+      className="message_root"
+    >
+      <Grid item>
+        {messages &&
+          messages.length !== 0 &&
+          messages.map((msg, index) => (
+            <>
+              <Grid item>
+                <p className="message_name">{msg.name}</p>
+              </Grid>
+              <Grid
+                container
+                direction="row"
+                alignItems="center"
+                justify="flex-start"
+              >
+                <Grid item className="message_ImageRoot">
+                  <Image
+                    src={
+                      msg.photo
+                        ? `${configs.PUBLIC_FILES_PATH}/${msg.photo}`
+                        : defaultProfile
+                    }
+                    addStyles="message_Image"
+                  />
+                </Grid>
+                <Grid item className="message_messageOuter">
+                  <Grid container direction="row" wrap="nowrap">
+                    <Grid
+                      item
+                      className="message_Content"
+                      style={{
+                        backgroundColor:
+                          user.id == msg.id
+                            ? colorscheme.red4
+                            : colorscheme.blue,
+                      }}
+                    >
+                      <span style={{ color: colorscheme.white }}>
+                        {msg.text}
+                      </span>
+                      <div ref={messagesEndRef} />
+                      <Grid item className="message_timeContainer">
+                        <p className="message_time">
+                          {format(new Date(msg.sentTime), "hh:mm aaa")}
+                        </p>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </>
+          ))}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Message;
