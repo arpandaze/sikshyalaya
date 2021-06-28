2c74d5716c2196fdb371c04e160d0390c76afb20
javaclll <yugu.upadhyaya@gmail.com>
2021-06-28 12:48:20 +0545

    crude to get answer by quiz_id implemented

--- backend/cruds/quiz_answer.py
+++ backend/cruds/quiz_answer.py
@@ -1,10 +1,17 @@
 from cruds.base import CRUDBase
 from schemas import QuizAnswerCreate, QuizAnswerUpdate
 from models import QuizAnswer
+from sqlalchemy.orm import Session
 
 
 class CRUDQuizAnswer(CRUDBase[QuizAnswer, QuizAnswerCreate, QuizAnswerUpdate]):
-    pass
+    def get_by_quiz_id(self, db: Session, *, quizId: int, studentId: int):
+        return (
+            db.query(self.model)
+            .filter_by(quiz_id=quizId)
+            .filter_by(student_id=studentId)
+            .first()
+        )
 
 
 crud_quiz_answer = CRUDQuizAnswer(QuizAnswer)
