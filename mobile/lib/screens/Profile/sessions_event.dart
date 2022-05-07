c526315dbc2055b3647d20290f3761767021902f
waterlulu12 <aatishshrestha123@gmail.com>
2022-05-07 15:07:54 +0545

    readded session and password qr login changes

---
+++ mobile/lib/screens/Profile/sessions_event.dart
@@ -0,0 +1,16 @@
+part of 'sessions_bloc.dart';
+
+abstract class SessionsEvent extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class GetSession extends SessionsEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class LogOutSession extends SessionsEvent {
+  @override
+  List<Object> get props => [];
+}
