c6eeeac472bf9c00557d17f0cbdbea3cc9055e4b
Daze <dazehere@yandex.com>
2022-05-07 15:03:16 +0545

    fix: Web prod config

--- web/src/utils/configs.jsx
+++ web/src/utils/configs.jsx
@@ -1,12 +1,12 @@
 const getConfig = () => {
   switch (process.env.REACT_APP_TARGET_BUILD_ENV) {
     case "prod":
-      return {
-        API_HOST: "http://localhost:8181",
-        WEBSOCKET_HOST: "ws://localhost:8181",
+    return {
+        API_HOST: "http://sikshyalaya.centralindia.cloudapp.azure.com:8080",
+        WEBSOCKET_HOST: "ws://sikshyalaya.centralindia.cloudapp.azure.com:8080",
         PRIVATE_ROUTE_ACCESS: false,
         AUTO_REDIRECT: true,
-        PUBLIC_FILES_PATH: "http://localhost:8081",
+        PUBLIC_FILES_PATH: "http://sikshyalaya.centralindia.cloudapp.azure.com:8081",
         USER_TYPES: {
           SUPERADMIN: 1,
           ADMIN: 2,
@@ -14,20 +14,6 @@ const getConfig = () => {
           STUDENT: 4,
         },
       };
-    // return {
-    //   API_HOST: "http://sikshyalaya.centralindia.cloudapp.azure.com:8080",
-    //   WEBSOCKET_HOST: "ws://sikshyalaya.centralindia.cloudapp.azure.com:8080",
-    //   PRIVATE_ROUTE_ACCESS: false,
-    //   AUTO_REDIRECT: true,
-    //   PUBLIC_FILES_PATH:
-    //     "http://sikshyalaya.centralindia.cloudapp.azure.com:8081",
-    //   USER_TYPES: {
-    //     SUPERADMIN: 1,
-    //     ADMIN: 2,
-    //     TEACHER: 3,
-    //     STUDENT: 4,
-    //   },
-    // };
 
     case "nondock":
       return {
